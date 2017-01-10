5c5e1d55b91614b27b00f0a75bd20b5997955796
jixiuf <jixiuf@qq.com>
2017-01-10 23:18:35 +0800

    update locate.sh

--- bin/locate.sh
+++ bin/locate.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# locate foo |grep bar |grep -v Foo ========= ./everything.sh foo bar @Foo
-cmd="locate -i $1"
+# locate foo |grep bar |grep -v Foo ========= ./locate.sh foo bar @Foo
+if [ `uname -s` = "Darwin" ] ; then
+    # macos
+    cmd="mdfind -name $1"
+else
+    cmd="locate -i $1"
+
+fi
+
 while [ $# -gt 1 ];
 do
     if [[ $2 =~ @(.*) ]]; then
-        cmd="$cmd | grep -i -v ${BASH_REMATCH[1]}";
+        if [ "" != "${BASH_REMATCH[1]}" ]; then
+            cmd="$cmd | grep -i -v ${BASH_REMATCH[1]}";
+        fi
     else
         cmd="$cmd | grep -i $2";
     fi
