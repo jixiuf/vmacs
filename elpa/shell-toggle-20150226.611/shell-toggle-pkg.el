66349eeb31ee4e5bfd2b0b54b44a144dc3a8baac
jixiuf <jixiuf@qq.com>
2017-01-14 19:04:51 +0800

    shell-toggle

---
+++ elpa/shell-toggle-20150226.611/shell-toggle-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "shell-toggle" "20150226.611" "Toggle to and from the shell buffer" 'nil :url "https://github.com/knu/shell-toggle.el" :keywords '("processes"))
