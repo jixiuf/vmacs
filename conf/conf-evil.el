2f86ae5ff33d9d264358ed5432ab209910cda97b
jixiuf <jixiuf@qq.com>
2017-01-22 13:40:18 +0800

    not for w32

--- conf/conf-evil.el
+++ conf/conf-evil.el
@@ -128,15 +128,16 @@
 
 (evil-mode 1)
 
-(require 'evil-terminal-cursor-changer)
-(defun vmacs-change-cursor-hook(&optional f)
-  (if (display-graphic-p)
-      (evil-terminal-cursor-changer-deactivate)
-    (evil-terminal-cursor-changer-activate)
-    (remove-hook 'pre-command-hook 'etcc--evil-set-cursor)))
+(unless (equal system-type 'windows-nt)
+  (require 'evil-terminal-cursor-changer)
+  (defun vmacs-change-cursor-hook(&optional f)
+    (if (display-graphic-p)
+        (evil-terminal-cursor-changer-deactivate)
+      (evil-terminal-cursor-changer-activate)
+      (remove-hook 'pre-command-hook 'etcc--evil-set-cursor)))
 
-(add-hook 'focus-in-hook 'vmacs-change-cursor-hook)
-(add-hook 'evil-insert-state-entry-hook 'vmacs-change-cursor-hook)
+  (add-hook 'focus-in-hook 'vmacs-change-cursor-hook)
+  (add-hook 'evil-insert-state-entry-hook 'vmacs-change-cursor-hook))
 
 
 (global-evil-matchit-mode 1)
