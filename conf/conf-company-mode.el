df175dac2250e141a19dbba5d7233fa8e257a525
jixiuf <jixiuf@qq.com>
2017-03-12 23:31:47 +0800

    vmacs-company-complete-common-or-selection

--- conf/conf-company-mode.el
+++ conf/conf-company-mode.el
@@ -1,5 +1,6 @@
 ;; (eval-when-compile (require 'company))
 
+
 (with-eval-after-load 'company
   ;; 调整默认backends
   (setq-default company-backends `(,@(unless (version< "24.3.51" emacs-version)
@@ -26,7 +27,8 @@
                                    ;; company-bbdb
                                    ))
 
-  (setq-default company-idle-delay 0.08)
+  (setq-default company-idle-delay 0.2)
+  (setq company-echo-delay 0)
   (setq-default company-minimum-prefix-length 2)
   ;; (define-key company-mode-map (kbd "C-i") 'company-other-backend) ;iterm map to C-i
   ;; (define-key company-mode-map (kbd "C-[ [ a h") (key-binding (kbd "C-i"))) ;iterm map to C-i
@@ -34,11 +36,21 @@
   (define-key company-active-map (kbd "C-s") #'company-filter-candidates)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map [tab] 'vmacs-company-complete-common-or-selection)
+  (define-key company-active-map (kbd "TAB") 'vmacs-company-complete-common-or-selection)
+  (define-key company-active-map [(meta tab)] 'company-complete-common)
+
   ;; (define-key company-mode-map (kbd "C-:") 'helm-company)
   ;; (define-key company-active-map (kbd "C-:") 'helm-company)
   )
 (global-company-mode 1)
 ;; (add-hook 'after-init-hook (lambda() (global-company-mode 1)))
+
+(defun vmacs-company-complete-common-or-selection()
+  (interactive)
+  (call-interactively 'company-complete-common)
+  (when (equal company-prefix company-common)
+    (call-interactively 'company-complete-selection)))
 
 (provide 'conf-company-mode)
 
