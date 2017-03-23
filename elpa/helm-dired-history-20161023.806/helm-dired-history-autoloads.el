7db0533f98f65d2099ee641b7f8a726589ab5c35
jixiuf <jixiuf@qq.com>
2017-03-23 14:33:23 +0800

    upgrade elpa

--- elpa/helm-dired-history-20161023.806/helm-dired-history-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; helm-dired-history-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "helm-dired-history" "helm-dired-history.el"
-;;;;;;  (22716 3640 0 0))
-;;; Generated autoloads from helm-dired-history.el
-
-(autoload 'helm-dired-history-view "helm-dired-history" "\
-call `helm' to show dired history.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; helm-dired-history-autoloads.el ends here
