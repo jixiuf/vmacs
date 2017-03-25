dfd29af7c89ae142a1fe2eb5c160e91ffbe62b06
jixiuf <jixiuf@qq.com>
2017-03-25 11:26:36 +0800

    exec-path fix

--- conf/conf-macos.el
+++ conf/conf-macos.el
@@ -29,8 +29,10 @@
 ;; (setenv "PATH" (concat  "/usr/local/bin:" (getenv "PATH") ))
 ;; 从shell 中获取环境变量
 (setq exec-path-from-shell-variables '("PATH" "MANPATH" "GOROOT" "GOPATH" "EDITOR" "PYTHONPATH"))
-(setq exec-path-from-shell-check-startup-files nil)
-(setq exec-path-from-shell-arguments '("-l" "-i")) ;remove -i read form .zshenv
+
+;; 设成nil 则不从 .zshrc 读 只从 .zshenv读（可以加快速度，但是需要你将环境变量相关的都放到 .zshenv 中，而非 .zshrc 中）
+(setq exec-path-from-shell-check-startup-files nil) ;
+(setq exec-path-from-shell-arguments '("-l" )) ;remove -i read form .zshenv
 (exec-path-from-shell-initialize)
 
 (setenv "LANG" "zh_CN.UTF-8")
