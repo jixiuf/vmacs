71c905a83dc174234c250ecbeca30215f28a0b60
jixiuf <jixiuf@qq.com>
2017-05-05 21:58:49 +0800

    upgrade

--- elpa/erlang-20170309.216/erlang-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "erlang" "20170309.216" "Erlang major mode"
-  '((emacs "24.1")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/web-mode-20170410.1335/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20170410.1335" "major mode for editing web templates" '((emacs "23.1")) :url "http://web-mode.org" :keywords '("languages"))
