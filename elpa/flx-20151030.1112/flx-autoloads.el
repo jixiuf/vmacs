cd044df376b22672893748ce0805e48b7f2872cb
jixiuf <jixiuf@qq.com>
2017-06-15 13:31:30 +0800

    flx

---
+++ elpa/flx-20151030.1112/flx-autoloads.el
@@ -0,0 +1,18 @@
+;;; flx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "flx" "flx.el" (0 0 0 0))
+;;; Generated autoloads from flx.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flx" '("flx-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flx-autoloads.el ends here
