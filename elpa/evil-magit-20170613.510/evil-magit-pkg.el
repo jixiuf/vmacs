327c35a38c9476a3d8e99d5fb64bc5dcc8ef60fc
jixiuf <jixiuf@qq.com>
2017-06-16 10:28:33 +0800

    upgrade

---
+++ elpa/evil-magit-20170613.510/evil-magit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-magit" "20170613.510" "evil-based key bindings for magit" '((evil "1.2.3") (magit "2.6.0")) :commit "91c96e6ffdc6e7d5e49fd9b0ffb7df8794f0f2db" :url "https://github.com/justbur/evil-magit")
---
+++ elpa/markdown-mode-20170615.1216/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20170615.1216" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "9cbec2c10b9d89f494ef76e6e18b77fa62ff5089" :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
