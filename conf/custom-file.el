ac05706a4f64a327fe8cd98c21649c0c8549a333
jixiuf <jixiuf@qq.com>
2017-06-16 17:45:32 +0800

    no terminal change cursor

--- conf/custom-file.el
+++ conf/custom-file.el
@@ -380,7 +380,7 @@
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (ivy-dired-history counsel flx magit gitconfig-mode shell-toggle company-jedi dired-filetype-face company-go auto-compile golden-ratio-scroll-screen company evil-textobj-anyblock exec-path-from-shell applescript-mode async bm crontab-mode dockerfile-mode erlang ethan-wspace evil evil-leader evil-magit evil-terminal-cursor-changer flycheck git-commit go-eldoc go-mode golden-ratio goto-chg helm-core iedit logstash-conf lua-mode markdown-mode protobuf-mode thrift web-mode wgrep with-editor yaml-mode yasnippet)))
+    (ivy-dired-history counsel flx magit gitconfig-mode shell-toggle company-jedi dired-filetype-face company-go auto-compile golden-ratio-scroll-screen company evil-textobj-anyblock exec-path-from-shell applescript-mode async bm crontab-mode dockerfile-mode erlang ethan-wspace evil evil-leader evil-magit flycheck git-commit go-eldoc go-mode golden-ratio goto-chg helm-core iedit logstash-conf lua-mode markdown-mode protobuf-mode thrift web-mode wgrep with-editor yaml-mode yasnippet)))
  '(recentf-save-file "~/.emacs.d/cache/recentf")
  '(safe-local-variable-values
    (quote
