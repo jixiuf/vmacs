7a916bf42f77c41e6092768d2d12ed6f9d5f1891
jixiuf <jixiuf@qq.com>
2017-07-21 12:35:15 +0800

    upgrade

--- elpa/counsel-20170710.1111/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20170710.1111" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "77f51fc69b2f859820312643d286013f9d3c430a" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
---
+++ elpa/markdown-mode-20170712.1703/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20170712.1703" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "ea5549233b3ce1536dae3a4793df79971a3781da" :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
