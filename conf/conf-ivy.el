e816b01c190286e5c2c526454eabefd2573da1c6
jixiuf <jixiuf@qq.com>
2017-08-30 21:53:55 +0800

    (global-set-key (kbd "C-S-M-s-i") 'counsel-git)   ;hyper-i

--- conf/conf-ivy.el
+++ conf/conf-ivy.el
@@ -92,6 +92,7 @@
 ;; gui 下让ctrl-i与tab 不同
 (global-set-key (kbd "<C-i>") 'counsel-git) ;Ctrl-i not tab
 (global-set-key (kbd "s-i") 'counsel-git)   ;cmd-i
+(global-set-key (kbd "C-S-M-s-i") 'counsel-git)   ;hyper-i
 
 (define-key ivy-minibuffer-map (kbd "<C-i>") 'vmacs-ivy-dropto-counsel-git)
 
