32e0f7d90d0370252466f3e2c81d9174dc1bec4c
jixiufeng <jixiufeng@luojilab.com>
2017-09-14 15:31:57 +0800

    (setq exec-path-from-shell-shell-name "zsh")

--- conf/conf-macos.el
+++ conf/conf-macos.el
@@ -11,6 +11,7 @@
 (setq exec-path-from-shell-variables '("PATH" "MANPATH" "GOROOT" "GOPATH" "EDITOR" "PYTHONPATH"))
 
 ;; 设成nil 则不从 .zshrc 读 只从 .zshenv读（可以加快速度，但是需要你将环境变量相关的都放到 .zshenv 中，而非 .zshrc 中）
+(setq exec-path-from-shell-shell-name "zsh")
 (setq exec-path-from-shell-check-startup-files nil) ;
 (setq exec-path-from-shell-arguments '("-l" )) ;remove -i read form .zshenv
 (exec-path-from-shell-initialize)
