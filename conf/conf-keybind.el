1f8e3254491c7b92130dc284b28ee5b010c09398
jixiufeng <jixiufeng@luojilab.com>
2018-01-05 00:30:01 +0800

    cmd-d

--- conf/conf-keybind.el
+++ conf/conf-keybind.el
@@ -42,7 +42,7 @@
 
 (global-set-key (kbd "C-<f3>") 'cd-iterm2)
 (global-set-key (kbd "<f3>") 'cd-iterm2)
-(global-set-key  (kbd "s-C-S-M-d") 'cd-iterm2)
+;; (global-set-key  (kbd "s-C-S-M-d") 'cd-iterm2)
 ;; 在minibuffer用C-l用于回到上层目录，通常在打开文件时用的到
 (define-key minibuffer-local-completion-map (kbd "C-l") 'minibuffer-up-parent-dir)
 (define-key minibuffer-local-map (kbd "C-l") 'minibuffer-up-parent-dir)
