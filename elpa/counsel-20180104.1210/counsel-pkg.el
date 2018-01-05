0c5e597eb504f68868e03d76db8c49089fb1c7c8
jixiufeng <jixiufeng@luojilab.com>
2018-01-05 22:29:32 +0800

    upgrade

---
+++ elpa/counsel-20180104.1210/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20180104.1210" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "7042d7096e440ef392fe39aeb8e0c7be957151b5" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
