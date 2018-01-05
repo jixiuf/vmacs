484e789e7fb6c7ddf58c3d5d56536b334b449a39
jixiufeng <jixiufeng@luojilab.com>
2018-01-05 22:33:10 +0800

    upgrade

--- elpa/markdown-mode-20171222.927/markdown-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "markdown-mode" "20171222.927" "Major mode for Markdown-formatted text" '((emacs "24.4") (cl-lib "0.5")) :commit "faf60003180bc9db5e69fcef08040f544217d08f" :url "https://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
