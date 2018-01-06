7a6e9c00e718a7872c0541b4a0b1c1636a7daf1b
jixiufeng <jixiufeng@luojilab.com>
2018-01-06 17:08:03 +0800

    auto save 1s

--- conf/custom-file.el
+++ conf/custom-file.el
@@ -349,6 +349,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-visited-interval 1)
  '(auto-save-visited-mode t)
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/cache/backup_files/"))))
  '(blink-cursor-mode t)
@@ -365,7 +366,7 @@
  '(magit-push-arguments (quote ("--force-with-lease")))
  '(magit-save-repository-buffers (quote dontask))
  '(menu-bar-mode nil)
- '(org-agenda-files nil)
+ '(org-agenda-files nil t)
  '(package-selected-packages
    (quote
     (ctable dash deferred osx-dictionary dired-narrow smex ivy-dired-history counsel flx magit gitconfig-mode company-jedi dired-filetype-face company-go auto-compile golden-ratio-scroll-screen company evil-textobj-anyblock exec-path-from-shell applescript-mode async bm crontab-mode dockerfile-mode erlang ethan-wspace evil evil-leader evil-magit flycheck git-commit go-eldoc go-mode golden-ratio goto-chg helm-core logstash-conf lua-mode markdown-mode protobuf-mode thrift web-mode wgrep with-editor yaml-mode yasnippet)))
@@ -446,7 +447,7 @@
  '(save-place-mode t)
  '(savehist-file "~/.emacs.d/cache/history")
  '(scroll-bar-mode nil)
- '(tramp-persistency-file-name "~/.emacs.d/cache/tramp")
+ '(tramp-persistency-file-name "~/.emacs.d/cache/tramp" nil (tramp))
  '(tramp-syntax (quote default) nil (tramp))
  '(uniquify-buffer-name-style (quote forward) nil (uniquify))
  '(warning-suppress-types (quote ((yasnippet backquote-change))))
