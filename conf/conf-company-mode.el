0a35718fcd12b7509110a4470dd7a498f614bb9c
jixiufeng <jixiufeng@luojilab.com>
2018-01-13 15:09:50 +0800

    update company

--- conf/conf-company-mode.el
+++ conf/conf-company-mode.el
@@ -78,6 +78,30 @@
   (setq this-command 'company-complete-selection)
     (call-interactively 'company-complete-selection)))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 扶持使用数字选candidate
+(setq company-show-numbers t)
+(let ((map company-active-map))
+  (mapc
+   (lambda (x)
+     (define-key map (format "%d" x) 'vmacs-company-number))
+   (number-sequence 0 9)))
+
+(defun vmacs-company-number ()
+  "Forward to `company-complete-number'.
+
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+  (interactive)
+  (let* ((k (this-command-keys))
+         (re (concat "^" company-prefix k)))
+    (if (cl-find-if (lambda (s) (string-match re s))
+                    company-candidates)
+        (self-insert-command 1)
+      (company-complete-number (string-to-number k)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'conf-company-mode)
 
 ;; Local Variables:
