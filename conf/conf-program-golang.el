b71094f2a69e4e674bae069f7d13ac0e43e0b410
jixiufeng <jixiufeng@luojilab.com>
2018-01-14 11:56:10 +0800

    go import

--- conf/conf-program-golang.el
+++ conf/conf-program-golang.el
@@ -1,5 +1,7 @@
 ;; # (-u flag for "update")
 ;; go get -u github.com/nsf/gocode
+;; gocode set
+;; gocode set autobuild true
 ;; windows
 ;; go get -u -ldflags -H=windowsgui github.com/nsf/gocode
 ;; go get -u github.com/dougm/goflymake
@@ -57,9 +59,24 @@
   (setq require-final-newline nil)
   (modify-syntax-entry ?_  "_" (syntax-table)) ;还是让 "_" 作为symbol，还不是word
   (flycheck-mode 1)
+  (local-set-key (kbd "C-c C-a") 'go-imports-insert-import)
+  (local-set-key (kbd "C-c C-r") 'go-imports-reload-packages-list)
   (local-set-key (kbd "C-c i") 'go-goto-imports)
-  (local-set-key (kbd "C-c g") 'golang-setter-getter)
-  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports))
+  (local-set-key (kbd "C-c g") 'golang-setter-getter))
+
+
+(defun vmacs-auto-build-package()
+  (interactive)
+  (require 'go-imports)
+  (go-imports-reload-packages-list)
+  (unless (get-buffer-process  " *go-install*")
+    (set-process-query-on-exit-flag
+     (start-process-shell-command
+      "go-install-generate-shell" " *go-install*"
+      (format "perl %s ~/go |cut -d '\"' -f 4|grep -v vendor|sort|uniq|sed 's/^/go install /g'|sh"
+              go-imports-find-packages-pl-path))nil)))
+
+(run-with-idle-timer (* 10 1) t 'vmacs-auto-build-package)
 
 (provide 'conf-program-golang)
 
---
+++ elpa/go-imports-20180107.1423/go-imports-pkg.el
@@ -0,0 +1,5 @@
+(define-package "go-imports" "20180107.1423" "Insert go import statement given package name" 'nil :url "https://github.com/yasushi-saito/go-imports" :keywords
+  '("tools" "go" "import"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
