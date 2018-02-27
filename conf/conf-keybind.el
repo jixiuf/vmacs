e848406580f0d5390ee5f399e2785e6f637dee08
jixiufeng <jixiufeng@luojilab.com>
2018-02-27 10:53:44 +0800

    (global-set-key  (kbd "C-\\") 'hippie-expand)

--- conf/conf-keybind.el
+++ conf/conf-keybind.el
@@ -99,6 +99,7 @@
 
 (global-set-key  (kbd "C-,") 'scroll-other-window-down)
 (global-set-key  (kbd "C-.") 'scroll-other-window)
+(global-set-key  (kbd "C-\\") 'hippie-expand)
 
 (global-set-key  (kbd "s-q") 'save-buffers-kill-emacs)
 (global-set-key  (kbd "s-C-S-M-q") 'save-buffers-kill-emacs)
