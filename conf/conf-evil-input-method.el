3935335b478d6b3918570a90a4cde8af6072db6c
jixiufeng <jixiufeng@luojilab.com>
2018-07-01 15:43:54 +0800

    evil input method

--- conf/conf-evil-input-method.el
+++ conf/conf-evil-input-method.el
@@ -14,20 +14,20 @@
 ;; https://github.com/rime/squirrel/wiki/如何实现mac下删除默认的英文输入法（只保留一个Squirrel)
 ;; https://raw.githubusercontent.com/jixiuf/dotfiles/master/mac/com.apple.HIToolbox.plist
 
-(defun disable-input-method-hook()
-  ;; (start-process "squirrel-input-method-disable-chinese" nil "/Library/Input Methods/Squirrel.app/Contents/MacOS/squirrel_client" "-s" "ascii_mode" "--clear")
-  (when (and (equal system-type 'darwin)
-             (member evil-previous-state '(insert emacs))
-             (file-exists-p "/Library/Input Methods/Squirrel.app/Contents/MacOS/squirrel_client" )) ;mac 上squirrel与输入法相关
-    (call-process  "/Library/Input Methods/Squirrel.app/Contents/MacOS/squirrel_client" nil nil nil "-s" "ascii_mode" )))
+;; (defun disable-input-method-hook()
+;;   ;; (start-process "squirrel-input-method-disable-chinese" nil "/Library/Input Methods/Squirrel.app/Contents/MacOS/squirrel_client" "-s" "ascii_mode" "--clear")
+;;   (when (and (equal system-type 'darwin)
+;;              (member evil-previous-state '(insert emacs))
+;;              (file-exists-p "/Library/Input Methods/Squirrel.app/Contents/MacOS/squirrel_client" )) ;mac 上squirrel与输入法相关
+;;     (call-process  "/Library/Input Methods/Squirrel.app/Contents/MacOS/squirrel_client" nil nil nil "-s" "ascii_mode" )))
 
-;; (defadvice keyboard-quit (before disable-input-method activate)
-;;   "disable-input-method-hook."
-;;   (disable-input-method-hook))
+;; ;; (defadvice keyboard-quit (before disable-input-method activate)
+;; ;;   "disable-input-method-hook."
+;; ;;   (disable-input-method-hook))
 
-;; 输入法进入normal state时，关闭输入法的中文模式
-(add-hook 'evil-normal-state-entry-hook 'disable-input-method-hook)
-(add-hook 'evil-motion-state-entry-hook 'disable-input-method-hook)
+;; ;; 输入法进入normal state时，关闭输入法的中文模式
+;; (add-hook 'evil-normal-state-entry-hook 'disable-input-method-hook)
+;; (add-hook 'evil-motion-state-entry-hook 'disable-input-method-hook)
 (global-set-key (kbd "<f17>") 'evil-normal-state) ;mac karabiner用来控制输入法
 (define-key isearch-mode-map (kbd "<f17>") 'evil-normal-state) ;详见isearch-pre-command-hook
 
@@ -35,12 +35,13 @@
 ;; https://github.com/jixiuf/dotfiles/blob/master/mac/hammerspoon/init.lua
 ;; 当切换输入法到中文状态时的时候会回调到这里
 ;;回调的时候有可能是上面disable-input-method-hook 里调，此时不应该切到insert模式
-(defun my-evil-insert()
-  (interactive)
-  (unless (equal 'evil-normal-state last-command)
-    (evil-insert-state)))
-(global-set-key (kbd "<f18>") 'my-evil-insert) ;mac karabiner用来控制输入法
-(define-key isearch-mode-map (kbd "<f18>") 'my-evil-insert) ;详见isearch-pre-command-hook
+;; (defun my-evil-insert()
+;;   (interactive)
+;;   (message "hello")
+;;   (unless (equal 'evil-normal-state last-command)
+;;     (evil-insert-state)))
+(global-set-key (kbd "<f18>") 'evil-insert-state) ;mac karabiner用来控制输入法
+(define-key isearch-mode-map (kbd "<f18>") 'evil-insert-state) ;详见isearch-pre-command-hook
 ;; "Non-nil means random control characters terminate incremental search."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'conf-evil-input-method)
