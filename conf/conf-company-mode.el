48c45ced2463cfd7db4aceb48df419638e09ed24
jixiufeng <jixiufeng@luojilab.com>
2018-07-25 11:23:17 +0800

    update company

--- conf/conf-company-mode.el
+++ conf/conf-company-mode.el
@@ -29,6 +29,10 @@
                 ))
 
 
+   ;; 'all 的意思是像 dabbrev-expand 那样搜索所有 buffer 的内容, 而不仅仅是和当前模式相同的buffer里面去搜索.
+  (setq-default company-dabbrev-code-other-buffers 'all)
+  ;; company-dabbrev-char-regexp 默认的正则表达式是 \sw , 意味着只能匹配单词才能补全, 我重新定制成 [\.0-9a-z-_’/] , 增加搜寻的范围
+  (setq-default company-dabbrev-char-regexp "[\\.0-9a-z-_'/]") ;adjust regexp make `company-dabbrev' search words like `dabbrev-expand'
   (setq-default company-idle-delay 0.3)
   (setq company-echo-delay 0)
   (setq company-tooltip-minimum-width 20)
