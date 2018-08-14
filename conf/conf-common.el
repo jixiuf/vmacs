4d0b2e661f9b56085c9c686e2ffc4c335cbd4f6c
jixiufeng <jixiufeng@luojilab.com>
2018-08-14 22:52:05 +0800

    frame-title-format

--- conf/conf-common.el
+++ conf/conf-common.el
@@ -18,7 +18,7 @@
 
  use-dialog-box nil		      ;不使用对话框进行（是，否 取消） 的选择，而是用minibuffer
  ;; frame-title-format "%b  [%I] %f  GNU/Emacs" ;标题显示文件名，而不是默认的username@localhost
- frame-title-format '("%e " evil-mode-line-format mode-line-buffer-identification " "   mode-line-client   mode-line-position (vc-mode vc-mode) "  "  mode-line-misc-info mode-line-end-spaces "%f  GNU/Emacs")
+ frame-title-format '("%e " evil-mode-line-format "「"mode-line-buffer-identification "」("  (:propertize ("" mode-name) ) ") "   mode-line-misc-info   "%f  GNU/Emacs")
 
  ;;  mode-line 上显示当前文件是什么系统的文件(windows 的换行符是\n\r)
  eol-mnemonic-dos "[w32]"
@@ -55,7 +55,7 @@
 
  ;; after this shell-command can use zsh alias
  ;; shell-file-name "zsh"
-;; shell-command-switch "-ic"
+ ;; shell-command-switch "-ic"
 
  ;;注意这两个变量是与recentf相关的,把它放在这里,是因为
  ;;觉得recentf与filecache作用有相通之处,
