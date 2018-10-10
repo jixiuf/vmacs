29ecb10e05d428d6371a1ef5b004e4fb95d92f10
jixiufeng <jixiufeng@luojilab.com>
2018-10-10 20:24:45 +0800

    custom term color

--- conf/custom-file.el
+++ conf/custom-file.el
@@ -173,6 +173,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#616161" "#ff8272" "#b4fa72" "#fefdc2" "#a5d5fe" "#ff8ffd" "#d0d1fe" "#f1f1f1"])
  '(auto-save-visited-interval 15)
  '(auto-save-visited-mode t)
  '(backup-directory-alist '((".*" . "~/.emacs.d/cache/backup_files/")))
