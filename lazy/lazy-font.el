319150ab9c29f9e67fe9a8c01c6734f0c1d9c2f1
jixiufeng <jixiufeng@luojilab.com>
2018-12-24 00:14:22 +0800

    (global-set-key [C-M-s-backspace] 'create-frame-font-large-mac)

--- lazy/lazy-font.el
+++ lazy/lazy-font.el
@@ -52,6 +52,25 @@
                     'hangul               ;script 까까까까까까까까까까까까까까까까까까까까
                     (font-spec :family "Apple SD Gothic Neo" :size 23)))
 
+
+;;;###autoload
+(defun create-frame-font-large-mac()          ;emacs 若直接启动 启动时调用此函数似乎无效
+  (interactive)
+  (set-face-attribute
+   'default nil :font "Menlo 20")
+  ;; Chinese Font
+  (dolist (charset '( han symbol cjk-misc bopomofo)) ;script 可以通过C-uC-x=查看当前光标下的字的信息
+    (set-fontset-font (frame-parameter nil 'font)
+                      charset
+                      (font-spec :family "PingFang SC" :size 24)))
+
+  (set-fontset-font (frame-parameter nil 'font)
+                    'kana                 ;script ｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺｺ
+                    (font-spec :family "Hiragino Sans" :size 24))
+  (set-fontset-font (frame-parameter nil 'font)
+                    'hangul               ;script 까까까까까까까까까까까까까까까까까까까까
+                    (font-spec :family "Apple SD Gothic Neo" :size 24)))
+
 ;;;###autoload
 (defun create-frame-font-mac()
   (interactive)
