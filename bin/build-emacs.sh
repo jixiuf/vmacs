f387e182268ed998de84cd7e587d26f1797bc456
jixiufeng <jixiufeng@luojilab.com>
2019-01-19 18:57:57 +0800

    add build-emacs.sh

---
+++ bin/build-emacs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+prefix=/usr/local/emacs
+echo ${prefix}
+export PATH=$PATH:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/gnu-sed/bin
+./configure \
+--disable-silent-rules \
+--enable-locallisppath=/usr/local/share/emacs/site-lisp \
+--prefix=${prefix} \
+--with-gnutls \
+--without-x \
+--without-dbus \
+--without-imagemagick \
+--with-modules \
+--with-ns \
+--without-xml2 \
+--disable-ns-self-contained
+make
+make install
+rm -rf ${prefix}/Emacs.app
+cp -rf nextstep/Emacs.app  ${prefix}/Emacs.app
+mv -f ${prefix}/bin/emacs ${prefix}/bin/emacsbak
+cat >${prefix}/bin/emacs << EOS
+#!/bin/bash
+exec ${prefix}/Emacs.app/Contents/MacOS/Emacs "\$@"
+EOS
+
+chmod 755 ${prefix}/bin/emacs
