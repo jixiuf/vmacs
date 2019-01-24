38d9548d311381eff5620aca2b31d62524bc86b2
jixiufeng <jixiufeng@luojilab.com>
2019-01-24 13:11:11 +0800

    fix posfrmae

--- conf/conf-company-mode.el
+++ conf/conf-company-mode.el
@@ -78,7 +78,12 @@
 
 (unless (daemonp)
   (company-posframe-mode 1))
-(add-hook 'after-make-frame-functions 'company-posframe-mode)
+
+(defun vmacs-company-posframe-mode(&optional frame)
+  (with-selected-frame frame
+    (company-posframe-mode 1)))
+
+(add-hook 'after-make-frame-functions 'vmacs-company-posframe-mode)
 ;; (add-hook 'after-init-hook (lambda() (global-company-mode 1)))
 
 (defun vmacs-company-complete-common-or-selection()
