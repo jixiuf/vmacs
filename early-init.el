1ab60cd15bf1a5d0ac24c8bf42a501414e640301
jixiufeng <jixiufeng@luojilab.com>
2019-02-09 22:20:37 +0800

    no gc setting

--- early-init.el
+++ early-init.el
@@ -10,7 +10,7 @@
 (setq load-prefer-newer t)              ;当el文件比elc文件新的时候,则加载el,即尽量Load最新文件文件
 ;; By default Emacs will initiate GC every 0.76 MB allocated (gc-cons-threshold == 800000).
 ;; If we increase this to 20 MB (gc-cons-threshold == 20000000) we get:
-(setq gc-cons-threshold  (* 300 1024 1024)) ;300 MB before garbage collection
+(setq gc-cons-threshold  (* 10 1024 1024)) ;50 MB before garbage collection
 ;; 禁用工具栏，滚运条 菜单栏
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
