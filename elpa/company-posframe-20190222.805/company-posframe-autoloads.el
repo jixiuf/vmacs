caae819ed398fcd7a91659d6ce93c5bd9c0473f9
jixiufeng <jixiufeng@luojilab.com>
2019-02-26 22:22:14 +0800

    upgrade

---
+++ elpa/company-posframe-20190222.805/company-posframe-autoloads.el
@@ -0,0 +1,43 @@
+;;; company-posframe-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "company-posframe" "company-posframe.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from company-posframe.el
+
+(defvar company-posframe-mode nil "\
+Non-nil if Company-Posframe mode is enabled.
+See the `company-posframe-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `company-posframe-mode'.")
+
+(custom-autoload 'company-posframe-mode "company-posframe" nil)
+
+(autoload 'company-posframe-mode "company-posframe" "\
+company-posframe minor mode.
+
+If called interactively, enable Company-Posframe mode if ARG is positive, and
+disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it
+if ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "company-posframe" '("company-posframe-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-posframe-autoloads.el ends here
