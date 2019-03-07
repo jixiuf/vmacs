0c24ec19f399cccf69f5faf9b42d81a7ff1d022c
jixiufeng <jixiufeng@luojilab.com>
2019-03-07 11:07:58 +0800

    fix fullscreen

--- conf/conf-macos.el
+++ conf/conf-macos.el
@@ -59,7 +59,20 @@
 
 ;; f11 (toggle-frame-fullscreen) default
 (setq ns-pop-up-frames nil)
-
+(setq frame-resize-pixelwise t)         ;;设置缩放的模式,避免Mac平台最大化窗口以后右边和下边有空隙
+;; 在Mac平台, Emacs不能进入Mac原生的全屏模式,否则会导致 `make-frame' 创建时也集成原生全屏属性后造成白屏和左右滑动现象.
+;; 所以先设置 `ns-use-native-fullscreen' 和 `ns-use-fullscreen-animation' 禁止Emacs使用Mac原生的全屏模式.
+;; 而是采用传统的全屏模式, 传统的全屏模式, 只会在当前工作区全屏,而不是切换到Mac那种单独的全屏工作区,
+;; 这样执行 `make-frame' 先关代码或插件时,就不会因为Mac单独工作区左右滑动产生的bug.
+;;
+;; Mac平台下,不能直接使用 `set-frame-parameter' 和 `fullboth' 来设置全屏,
+;; 那样也会导致Mac窗口管理器直接把Emacs窗口扔到单独的工作区, 从而对 `make-frame' 产生同样的Bug.
+;; 所以, 启动的时候通过 `set-frame-parameter' 和 `maximized' 先设置Emacs为最大化窗口状态, 启动5秒以后再设置成全屏状态,
+;; Mac就不会移动Emacs窗口到单独的工作区, 最终解决Mac平台下原生全屏窗口导致 `make-frame' 左右滑动闪烁的问题.
+(setq ns-use-native-fullscreen nil)
+(setq ns-use-fullscreen-animation nil)
+ ;; 默认先最大化。
+;; (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
 
 (defun term-enable-mouse-scroll(&optional f) ;
   (with-selected-frame f
