4b181a37518cb2f4bff7f6a3df7a0717c568f6e1
jixiufeng <jixiufeng@luojilab.com>
2019-04-16 22:30:59 +0800

    remove eglot

--- elpa/eglot-20190213.924/eglot-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/jixiuf/.emacs.d/elpa/eglot-20190213.924/eglot.el  -*- no-byte-compile: t -*-
-(define-package "eglot" "20190213.924" "Client for Language Server Protocol (LSP) servers" '((emacs "26.1") (jsonrpc "1.0.7") (flymake "1.0.5")) :commit "7d6e3cf5d7ae098aa6c8572343c8bc9b8453aace" :keywords '("convenience" "languages") :authors '(("João Távora" . "joaotavora@gmail.com")) :maintainer '("João Távora" . "joaotavora@gmail.com") :url "https://github.com/joaotavora/eglot")
