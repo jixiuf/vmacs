15f16322aea836534c468fa3bcf70bb604504485
jixiufeng <jixiufeng@luojilab.com>
2019-05-04 19:40:42 +0800

    upgrade

---
+++ elpa/company-lsp-20190501.1630/company-lsp-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-lsp" "20190501.1630" "Company completion backend for lsp-mode." '((emacs "25.1") (lsp-mode "6.0") (company "0.9.0") (s "1.2.0") (dash "2.11.0")) :commit "fef298347bb2c68f885b9fbb490a8932fa2a0759" :url "https://github.com/tigersoldier/company-lsp")
