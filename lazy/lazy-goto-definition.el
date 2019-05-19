be863594db2d75d4ac4c0dfe9bd5c9c03ec2f03f
jixiufeng <jixiufeng@luojilab.com>
2019-05-20 00:36:01 +0800

    (defun goto-definition (&optional arg)

--- lazy/lazy-goto-definition.el
+++ lazy/lazy-goto-definition.el
@@ -1,6 +1,5 @@
 (eval-when-compile (require 'cl-lib))
 
-(autoload 'find-library-name "find-func" "find libary")
 ;; 跳转到函数定义的地方，跳转前在原处设置一个书签,space,可回原处
 ;; default bind on gd
 ;;;###autoload
@@ -8,47 +7,25 @@
   "Make use of emacs' find-func and etags possibilities for finding definitions."
   (interactive "P")
   (let ((line (buffer-substring-no-properties
-               (line-beginning-position) (line-end-position)))
-        ;; (marker (point-marker))
-        )
+               (line-beginning-position) (line-end-position))))
     (bm-bookmark-add line nil t)        ;跳转前在原处加一个书签
     (cl-case major-mode
       (emacs-lisp-mode
        (elisp-def-mode 1)
-       (call-interactively 'elisp-def))
-      (lisp-interaction-mode
-       (elisp-def-mode 1)
-       (call-interactively 'elisp-def))
-      ;; (erlang-mode (erl-find-source-under-point))
-      ;; (c++-mode
-      ;;  (if (string-match "[ ]*#[ \t]*include[ \t]+[\"<]\\(.*\\)[\">]" line)
-      ;;      ;; for c++-mode ,in current line contains #include ,then try to open the include file using helm-gtags
-      ;;      (helm-gtags-find-files (match-string 1 line))
-      ;;    (helm-gtags-find-tag-and-symbol)))
-      ;; (c-mode
-      ;;  (if (string-match "[ ]*#[ \t]*include[ \t]+[\"<]\\(.*\\)[\">]" line)
-      ;;      ;; for c-mode ,in current line contains #include ,then try to open the include file using helm-gtags
-      ;;      (helm-gtags-find-files (match-string 1 line))
-      ;;    (helm-gtags-find-tag-and-symbol)))
-      ;; (lua-mode
-      ;;  (call-interactively 'helm-gtags-find-tag-and-symbol)
-      ;;  )
-      ;; (csharp-mode
-      ;;  (call-interactively 'helm-gtags-find-tag-and-symbol)
-      ;;  )
-      (js-mode
-       (if (functionp 'tern-find-definition)
-           (call-interactively 'tern-find-definition)
-         (helm-gtags-find-tag-and-symbol)))
-      (go-mode
-       ;;(xref-find-definitions (xref-backend-identifier-at-point (xref-find-backend)))
-       (condition-case nil (lsp-find-definition)
-         (error (bm-bookmark-remove))))
-      (python-mode
-       (condition-case nil (lsp-find-definition)
-         (error (bm-bookmark-remove))))
-      (otherwise
-       (lsp-find-definition))))
+       (condition-case nil (call-interactively 'elisp-def)
+         (error (bm-bookmark-remove)))))
+    (lisp-interaction-mode
+     (elisp-def-mode 1)
+     (condition-case nil (call-interactively 'elisp-def)
+       (error (bm-bookmark-remove))))
+    (js-mode
+     (if (functionp 'tern-find-definition)
+         (call-interactively 'tern-find-definition)
+       (lsp-find-definition)))
+    (otherwise
+     (condition-case nil (lsp-find-definition)
+       (error (bm-bookmark-remove))))))
+
   (setq this-command 'goto-definition))
 
 
