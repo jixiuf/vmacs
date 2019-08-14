61b3a9959d403bcfd33d3359f8904fb95bcbcc9b
jixiuf <jixiuf@qq.com>
2019-08-14 11:07:12 +0800

    upgrade

--- elpa/lsp-mode-20190813.451/lsp-mode-pkg.el
+++ elpa/lsp-mode-20190813.451/lsp-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-mode" "20190812.425" "LSP mode"
+(define-package "lsp-mode" "20190813.451" "LSP mode"
   '((emacs "25.1")
     (dash "2.14.1")
     (dash-functional "2.14.1")
--- elpa/magit-20190813.1640/magit-pkg.el
+++ elpa/magit-20190813.1640/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20190812.1330" "A Git porcelain inside Emacs."
+(define-package "magit" "20190813.1640" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20180527")
     (dash "20180910")
