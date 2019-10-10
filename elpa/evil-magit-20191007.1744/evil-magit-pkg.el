c36188ac391edefa03f5871236fcf80aaadd1b2d
jixiuf <jixiuf@qq.com>
2019-10-10 21:47:10 +0800

    upgrade

---
+++ elpa/evil-magit-20191007.1744/evil-magit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jixiuf/.emacs.d/elpa/evil-magit-20191007.1744/evil-magit.el  -*- no-byte-compile: t -*-
+(define-package "evil-magit" "20191007.1744" "evil-based key bindings for magit" '((evil "1.2.3") (magit "2.6.0")) :commit "1decef941f252bfd862be50d99bfbc0660dfa18c" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/evil-magit")
