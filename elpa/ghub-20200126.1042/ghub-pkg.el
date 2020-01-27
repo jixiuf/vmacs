f2b29507d38b53a98b1892cddf2b39d842966c2e
jixiuf <jixiuf@qq.com>
2020-01-27 22:24:45 +0800

    undotree ungrade

---
+++ elpa/ghub-20200126.1042/ghub-pkg.el
@@ -0,0 +1,7 @@
+(define-package "ghub" "20200126.1042" "Minuscule client libraries for Git forge APIs."
+  '((emacs "25.1")
+    (let-alist "1.0.5")
+    (treepy "0.1.1")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/projectile-20200119.1105/projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/jixiuf/.emacs.d/elpa/projectile-20200119.1105/projectile.el  -*- no-byte-compile: t -*-
-(define-package "projectile" "20200119.1105" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :commit "27a0da9cdc69dcfd5249aebb9ecfbe06d52b91b0" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/projectile")
