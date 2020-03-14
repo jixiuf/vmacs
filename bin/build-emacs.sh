68bf3c449197e5c34d86b9fb218e3e1ba58d2a13
jixiuf <jixiuf@qq.com>
2020-03-14 14:45:37 +0800

    # make bootstrap BYTE_COMPILE_EXTRA_FLAGS='--eval "(setq comp-speed 0)"'

--- bin/build-emacs.sh
+++ bin/build-emacs.sh
@@ -72,6 +72,7 @@ CC='clang' \
 # git clean -fdx
 # make bootstrap -j 4
 make -j 4
+# make bootstrap BYTE_COMPILE_EXTRA_FLAGS='--eval "(setq comp-speed 0)"'
 echo more info see INSTALL.REPO when compile error
 function catch_errors() {
     echo "script aborted, because of errors";
