d13a089ab79d9c749a2922a011a205d18e27e891
jixiuf <jixiuf@qq.com>
2020-06-30 22:47:25 +0800

    keybind

--- conf/conf-keybind.el
+++ conf/conf-keybind.el
@@ -150,10 +150,10 @@ they are in `bind-map-set-keys'."
 ;; (global-set-key (kbd "<f3>") 'vmacs-shell-toggle)
 ;; (global-set-key (kbd "C-<f3>") 'vmacs-eshell-term-toggle)
 ;; (global-set-key  (kbd "s-C-M-d") 'vmacs-shell-toggle)
-(global-set-key [f3] 'cd-iterm2)
+;; (global-set-key [f3] 'cd-iterm2)
 
-(global-set-key [f2] 'vterm-toggle)
-(global-set-key [C-f2] 'vterm-toggle-cd)
+;; (global-set-key [f2] 'vterm-toggle)
+;; (global-set-key [C-f2] 'vterm-toggle-cd)
 (global-set-key (kbd "s-,") 'vterm-toggle)
 ;; (global-set-key (kbd "s-d") 'vterm-toggle)
 ;; (global-set-key (kbd "s-C-M-d") 'vterm-toggle)
