e3d8c46975d82c8cebbe192831881da8cc3d4dac
jixiuf <jixiuf@qq.com>
2020-07-02 22:31:58 +0800

    remoe logstash

--- elpa/logstash-conf-20170524.1929/logstash-conf-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/jixiuf/.emacs.d/elpa/logstash-conf-20170524.1929/logstash-conf.el  -*- no-byte-compile: t -*-
-(define-package "logstash-conf" "20170524.1929" "basic mode for editing logstash configuration" 'nil :commit "4e127f9aec190786613445aa88efa307ff7c6748" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
