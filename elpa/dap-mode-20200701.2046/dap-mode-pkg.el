faa734ae1070cf342cd2de531ca12631d35fa778
jixiuf <jixiuf@qq.com>
2020-07-04 21:59:34 +0800

    dapmode

---
+++ elpa/dap-mode-20200701.2046/dap-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "dap-mode" "20200701.2046" "Debug Adapter Protocol mode"
+  '((emacs "25.1")
+    (dash "2.14.1")
+    (lsp-mode "6.0")
+    (dash-functional "1.2.0")
+    (bui "1.1.0")
+    (f "0.20.0")
+    (s "1.12.0")
+    (lsp-treemacs "0.1"))
+  :commit "34f21e1f514c8fb9998f5773c7f499edc406fc48" :keywords
+  '("languages" "debug")
+  :authors
+  '(("Ivan Yonchovski" . "yyoncho@gmail.com"))
+  :maintainer
+  '("Ivan Yonchovski" . "yyoncho@gmail.com")
+  :url "https://github.com/yyoncho/dap-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/hydra-20200608.1528/hydra-pkg.el
@@ -0,0 +1,13 @@
+(define-package "hydra" "20200608.1528" "Make bindings that stick around."
+  '((cl-lib "0.5")
+    (lv "0"))
+  :commit "8a9124f80b6919ad5288172b3e9f46c5332763ca" :keywords
+  '("bindings")
+  :authors
+  '(("Oleh Krehel" . "ohwoeowho@gmail.com"))
+  :maintainer
+  '("Oleh Krehel" . "ohwoeowho@gmail.com")
+  :url "https://github.com/abo-abo/hydra")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
