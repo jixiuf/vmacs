bdda73d86759d8811fec94ca174295de362819b0
jixiuf <jixiuf@qq.com>
2020-09-28 00:46:10 +0800

    iedit

--- conf/conf-iedit.el
+++ conf/conf-iedit.el
@@ -1,54 +1,28 @@
 ;;; Code:
 (autoload 'iedit-mode "iedit" nil t)
-(setq-default iedit-toggle-key-default nil)
-(global-set-key (kbd "C-;") 'evil-iedit-state-iedit-mode)
-(evil-define-state iedit
-  "`iedit state' interfacing iedit mode."
-  :tag " <E> "
-  :enable (normal)
-  :cursor box
-  :message "-- IEDIT --"
-  ;; force iedit mode
-  (if (evil-replace-state-p) (call-interactively 'iedit-mode)))
-(set-keymap-parent  evil-iedit-state-map evil-normal-state-map)
+(global-set-key (kbd "C-;") 'iedit-mode)
+(with-eval-after-load 'iedit
+  (add-hook 'iedit-mode-hook #'(lambda() (electric-pair-mode -1)))
+  (add-hook 'iedit-aborting-hook  #'electric-pair-mode)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode (kbd "C-;")  'iedit-quit)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode "t"   'iedit-show/hide-occurrence-lines)
 
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode "gg"  'iedit-goto-first-occurrence)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode "G"   'iedit-goto-last-occurrence)
 
-(defun evil-iedit-state-iedit-mode (&optional arg)
-  "Start `iedit-mode'."
-  (interactive "P")
-  (when (fboundp 'ahs-clear) (ahs-clear))
-  (when (bound-and-true-p electric-pair-mode)
-    (electric-pair-mode -1))
-  (iedit-mode arg)
-  (evil-iedit-state))
-;; (require 'evil-iedit-state)
-(define-key evil-iedit-state-map (kbd "C-;")  'evil-iedit-state-quit-iedit-mode)
-(define-key evil-iedit-state-map (kbd "C-g")  'evil-iedit-state-quit-iedit-mode)
-
-(defun evil-iedit-state-quit-iedit-mode ()
-  "Quit iedit-mode and return set state `evil-iedit-state-default-state'."
-  (interactive)
-  (electric-pair-mode 1)
-  (iedit-done)
-  (evil-normal-state))
-
-
-(define-key evil-iedit-state-map "t"   'iedit-show/hide-unmatched-lines)
-
-(define-key evil-iedit-state-map "gg"  'iedit-goto-first-occurrence)
-(define-key evil-iedit-state-map "G"   'iedit-goto-last-occurrence)
-
-(define-key evil-iedit-state-map "n"   'iedit-next-occurrence)
-(define-key evil-iedit-state-map "N"   'iedit-prev-occurrence)
-(define-key evil-iedit-state-map (kbd "TAB") 'iedit-toggle-selection)
-(define-key evil-iedit-state-map [tab]       'iedit-toggle-selection)
-(define-key evil-iedit-state-map  (kbd "gU") 'iedit-upcase-occurrences)
-(define-key evil-iedit-state-map  (kbd "gu") 'iedit-downcase-occurrences)
-(define-key evil-iedit-state-map  (kbd "mf") 'iedit-restrict-function)
-(define-key evil-iedit-state-map  (kbd "ml") 'iedit-restrict-current-line)
-(define-key evil-iedit-state-map  (kbd "zc") 'iedit-toggle-case-sensitive)
-(define-key evil-iedit-state-map  (kbd "zb") 'iedit-toggle-buffering)
-(define-key evil-iedit-state-map  (kbd "C－x r n") 'iedit-number-occurrences)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode "n"   'iedit-next-occurrence)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode "N"   'iedit-prev-occurrence)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode (kbd "TAB") 'iedit-toggle-selection)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode [tab]       'iedit-toggle-selection)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode  (kbd "gU") 'iedit-upcase-occurrences)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode  (kbd "gu") 'iedit-downcase-occurrences)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode  (kbd "mf") 'iedit-restrict-function)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode  (kbd "ml") 'iedit-restrict-current-line)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode  (kbd "zc") 'iedit-toggle-case-sensitive)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode  (kbd "zb") 'iedit-toggle-buffering)
+  (evil-define-minor-mode-key '(motion visual normal) 'iedit-mode  (kbd "C－x r n") 'iedit-number-occurrences)
+  (evil-normalize-keymaps) ;bug of evil-define-minor-mode-key https://github.com/emacs-evil/evil/issues/301
+  )
 
 (provide 'conf-iedit)
 
