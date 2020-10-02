d8d60c44b2e58990604a71b012f8ba2b06c6601d
jixiuf <jixiuf@qq.com>
2020-10-02 22:14:21 +0800

    adelete unused package

--- elpa/thrift-20200212.1903/thrift-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/jixiuf/.emacs.d/elpa/thrift-20200212.1903/thrift.el  -*- no-byte-compile: t -*-
-(define-package "thrift" "20200212.1903" "major mode for fbthrift and Apache Thrift files" '((emacs "24")) :commit "0ad2a772d7616e9a1923f0f1a5659131a869c1a3" :keywords '("languages"))
--- elpa/web-mode-20200820.1325/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/jixiuf/.emacs.d/elpa/web-mode-20200820.1325/web-mode.el  -*- no-byte-compile: t -*-
-(define-package "web-mode" "20200820.1325" "major mode for editing web templates" '((emacs "23.1")) :commit "3ecfe6b7f508556e935788e40784f254f8334837" :keywords '("languages") :authors '(("François-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("François-Xavier Bois") :url "http://web-mode.org")
