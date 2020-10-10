1d171020120dda91af5712eba62667ca3e7c1ccb
jixiuf <jixiuf@qq.com>
2020-10-10 11:19:31 +0800

    tarttime

--- early-init.el
+++ early-init.el
@@ -4,6 +4,32 @@
 ;;   \_/ |_| |_| |_|\__,_|\___|___/
 ;; https://emacs-china.org/t/macos-emacs-28-native-comp/12201/70?u=jixiuf
 ;; (setq comp-deferred-compilation-black-list '("yasnippet" "org-re-reveal" "tramp"))
+(defconst my/start-time (current-time))
+
+
+
+
+(defmacro mt (&rest body)
+  "Measure the time it takes to evaluate BODY."
+  `(let ((time (current-time)))
+     ,@body
+     (message "%.06f" (float-time (time-since time)))))
+
+(defvar file-name-handler-alist-old file-name-handler-alist)
+
+(setq file-name-handler-alist nil
+      message-log-max 16384
+      gc-cons-threshold most-positive-fixnum   ;; Defer Garbage collection
+      gc-cons-percentage 1.0)
+
+(add-hook 'window-setup-hook
+          (lambda ()
+            (setq file-name-handler-alist file-name-handler-alist-old
+                  gc-cons-threshold 800000
+                  gc-cons-percentage 0.1)
+	        (garbage-collect)
+	        (message "Load time %.06f" (float-time (time-since my/start-time))))
+	      t)
 
 (setq-default
  ;; user-full-name ""                ;记得改成你的名字
@@ -12,8 +38,8 @@
 (setq load-prefer-newer t)              ;当el文件比elc文件新的时候,则加载el,即尽量Load最新文件文件
 ;; By default Emacs will initiate GC every 0.76 MB allocated (gc-cons-threshold == 800000).
 ;; If we increase this to 20 MB (gc-cons-threshold == 20000000) we get:
-(setq gc-cons-threshold  (* 10 1024 1024)) ;50 MB before garbage collection
 (setq read-process-output-max (* 3 1024 1024)) ;; 3mb default 4k
+
 ;; 禁用工具栏，滚运条 菜单栏
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
