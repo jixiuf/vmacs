15683416bd341b1d512a7be288601ceff0089b9d
jixiuf <jixiuf@qq.com>
2020-10-12 16:10:36 +0800

    no miniframe

--- elpa/mini-frame-20201007.1902/mini-frame-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/jixiuf/.emacs.d/elpa/mini-frame-20201007.1902/mini-frame.el  -*- no-byte-compile: t -*-
-(define-package "mini-frame" "20201007.1902" "Show minibuffer in child frame on read-from-minibuffer" '((emacs "26.1")) :commit "0e5678d3cb56cb8569ebde413e9164ddef58ac7f" :keywords '("frames") :authors '(("Andrii Kolomoiets" . "andreyk.mad@gmail.com")) :maintainer '("Andrii Kolomoiets" . "andreyk.mad@gmail.com") :url "https://github.com/muffinmad/emacs-mini-frame")
