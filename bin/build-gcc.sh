a26f0e5d978fb1f5c88882e14dfb12cb860a89cc
jixiuf <jixiuf@qq.com>
2020-10-25 00:41:26 +0800

    do not need patch gcc

--- bin/build-gcc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-patchfile=${PWD}/gcc9.3.patch
-# cp $patchfile /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/
-cd /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/;git checkout ./Formula/gcc.rb; git apply $patchfile
-brew install --build-from-source gcc
