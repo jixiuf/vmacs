5c8d870b859a76ee11be9916ce7e0ac5e8b5a6fd
jixiuf <jixiuf@qq.com>
2020-12-12 11:58:16 +0800

    (when (require 'embark nil t) (define-key icomplete-fido-mode-map (kbd "C-o") 'embark-act))

--- conf/conf-icomplete.el
+++ conf/conf-icomplete.el
@@ -3,8 +3,6 @@
 
 ;; (setq orderless-component-separator " +")
 ;; (setq orderless-matching-styles '(orderless-regexp orderless-literal))
-
-;; (setq icomplete-delay-completions-threshold 0)
 ;; (setq icomplete-max-delay-chars 3)
 (setq icomplete-delay-completions-threshold 2000)
 (setq icomplete-compute-delay 0)
@@ -20,21 +18,14 @@
     (setq completion-styles '(orderless partial-completion basic substring initials flex))
   (setq completion-styles '(basic substring initials partial-completion flex)))
 
-(if (fboundp 'fido-mode)
-    (progn
-      (fido-mode 1)
-      (define-key icomplete-fido-mode-map (kbd "C-n") #'icomplete-forward-completions)
-      (define-key icomplete-fido-mode-map (kbd "C-p") #'icomplete-backward-completions)
-      (define-key icomplete-fido-mode-map (kbd "M-j") #'icomplete-force-complete-and-exit)
-      (define-key icomplete-fido-mode-map (kbd "C-l") #'icomplete-fido-backward-updir))
-  (icomplete-mode 1))
+(when (require 'embark nil t) (define-key icomplete-fido-mode-map (kbd "C-o") 'embark-act))
 
-
-;; (defun vmacs-fido-setup ())
-;; (add-hook 'minibuffer-setup-hook #'vmacs-fido-setup 99)
-
-(define-key icomplete-minibuffer-map (kbd "C-j") #'icomplete-fido-exit) ;minibuffer-complete-and-exit
-;; (define-key icomplete-fido-mode-map (kbd "SPC") #'self-insert-command)
+(fido-mode 1)
+(define-key icomplete-fido-mode-map (kbd "C-n") #'icomplete-forward-completions)
+(define-key icomplete-fido-mode-map (kbd "C-p") #'icomplete-backward-completions)
+(define-key icomplete-fido-mode-map (kbd "M-j") #'icomplete-force-complete-and-exit)
+(define-key icomplete-fido-mode-map (kbd "C-l") #'icomplete-fido-backward-updir)
+(define-key icomplete-fido-mode-map (kbd "C-j") #'icomplete-fido-exit) ;minibuffer-complete-and-exit
 
 (defadvice yank-pop (around icomplete-mode (arg) activate)
   (interactive "p")
