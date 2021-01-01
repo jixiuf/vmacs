dda019f0800a19f78493290ad8acc3b610579349
jixiuf <jixiuf@qq.com>
2021-01-02 00:36:31 +0800

    (vmacs-leader "gt" #'(lambda()(interactive) (consult-ripgrep (project-root (project-current t)))))

--- conf/conf-icomplete.el
+++ conf/conf-icomplete.el
@@ -51,7 +51,12 @@
 (fset 'imenu 'consult-imenu)
 (setq consult-async-default-split nil)
 (vmacs-leader "gg" #'consult-ripgrep)
-(vmacs-leader "g." #'consult-git-grep)
+(vmacs-leader "gt" #'(lambda()(interactive) (consult-ripgrep (project-root (project-current t)))))
+(vmacs-leader "g." #'(lambda()(interactive) (consult-ripgrep nil (thing-at-point 'symbol))))
+(vmacs-leader "g," #'(lambda()(interactive) (consult-ripgrep (project-root (project-current t)) (thing-at-point 'symbol))))
+(defun consult-ripgrep-project-root()
+  (let ((consult-project-root-function  #'(lambda() (project-root (project-current t)))) )
+    (call-interactively 'consult-ripgrep)))
 
 
 (defadvice yank-pop (around icomplete-mode (arg) activate)
