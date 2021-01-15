a50934b8fbfb82ca6b6aef1ab7875a0db652f1e2
jixiuf <jixiuf@qq.com>
2021-01-15 22:34:06 +0800

    nohideline

--- elpa/hide-lines-20151127.1840/hide-lines-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from hide-lines.el  -*- no-byte-compile: t -*-
-(define-package "hide-lines" "20151127.1840" "Commands for hiding lines based on a regexp" 'nil :commit "331122bf19361130351cfe55968c2a7820329eb3" :authors '(("Mark Hulme-Jones <ture at plig cucumber dot net>")) :maintainer '("Joe Bloggs" . "vapniks@yahoo.com") :keywords '("convenience") :url "https://github.com/vapniks/hide-lines")
