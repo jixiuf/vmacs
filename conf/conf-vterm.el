383c10fbd38afcaa9de20097f78a3b685ceb4092
jixiuf <jixiuf@qq.com>
2021-01-30 19:50:05 +0800

    vterm

--- conf/conf-vterm.el
+++ conf/conf-vterm.el
@@ -16,25 +16,27 @@
 (require 'vterm-toggle)
 
 (add-hook 'vterm-toggle-show-hook #'evil-insert-state)
-(add-hook 'vterm-toggle-hide-hook #'evil-insert-state)
+(add-hook 'vterm-toggle-hide-hook #'evil-normal-state)
 (setq vterm-toggle-fullscreen-p t)
 (setq vterm-toggle-reset-window-configration-after-exit 'kill-window-only)
 
 (defun vterm-ctrl-g ()
   "vterm ctrl-g"
   (interactive)
-  (if (or (save-excursion (goto-char (point-at-bol))(search-forward-regexp "filter>" nil t))
-          ;; (and (save-excursion (search-forward-regexp "[^\n \t]+" nil t))
-          ;;      (save-excursion (not (vterm-next-prompt))))
-          )
+  (if (save-excursion (goto-char (point-at-bol))(search-forward-regexp "filter>" nil t))
       (if (equal last-command 'vterm-ctrl-g)
           (evil-normal-state)
         (call-interactively 'vmacs-vterm-self-insert))
-    (if (equal last-command 'keyboard-quit)
+    (if (equal last-command 'vterm-copy-mode)
         (call-interactively 'vmacs-vterm-self-insert)
-      (setq this-command 'keyboard-quit)
-      (evil-normal-state)
-      (call-interactively 'keyboard-quit))))
+      (if (equal last-command 'evil-normal-state)
+          (progn
+            (vterm-copy-mode 1)
+            (setq this-command 'vterm-copy-mode)
+            )
+        (setq this-command 'evil-normal-state)
+        (evil-normal-state)))))
+
 
 (defun vmacs-vterm-self-insert()
   (interactive)
@@ -42,17 +44,17 @@
     (evil-insert-state))
   (call-interactively 'vterm--self-insert))
 
-(defun vmacs-vterm-disable-output()
-  (when (member major-mode '(vterm-mode))
-    (vterm-copy-mode 1)))
 (defun vmacs-vterm-enable-output()
   (when (member major-mode '(vterm-mode))
     (vterm-copy-mode -1)))
 
 (defun vmacs-vterm-copy-mode-hook()
   (if vterm-copy-mode
-      (unless (evil-normal-state-p)
-        (evil-normal-state))
+      (progn
+        (message "vterm-copy-mode enabled")
+        (unless (evil-normal-state-p)
+          (evil-normal-state)))
+
     (unless (evil-insert-state-p)
       (evil-insert-state))))
 
@@ -60,8 +62,6 @@
 
 
 (add-hook 'evil-insert-state-entry-hook 'vmacs-vterm-enable-output)
-(add-hook 'evil-normal-state-entry-hook 'vmacs-vterm-disable-output)
-
 
 (defun vterm-eob()
   (interactive)
@@ -70,7 +70,6 @@
 
 (define-key vterm-mode-map (kbd "s-C-M-u") 'vterm-toggle)
 ;; (define-key vterm-mode-map (kbd "s-t")   #'vterm-toggle-cd-show)
-(define-key vterm-mode-map (kbd "C-g")   #'vterm-ctrl-g)
 (define-key vterm-mode-map (kbd "C-c C-g")   #'vterm--self-insert)
 (define-key vterm-mode-map (kbd "s-v")   #'vterm-yank)
 (define-key vterm-mode-map [f2]   nil)
@@ -99,6 +98,7 @@
   (evil-define-key 'insert 'local [escape] 'vterm--self-insert)
   (evil-define-key 'motion'local (kbd "C-r") 'vmacs-vterm-self-insert)
   (evil-define-key 'insert 'local (kbd "C-g") 'vterm-ctrl-g)
+  (evil-define-key 'normal'local (kbd "C-g") 'vterm-ctrl-g)
   (evil-define-key 'normal 'local (kbd "C-p") 'vmacs-vterm-self-insert)
   (evil-define-key 'normal 'local (kbd "C-n") 'vmacs-vterm-self-insert)
   (evil-define-key 'normal 'local (kbd "C-r") 'vmacs-vterm-self-insert)
