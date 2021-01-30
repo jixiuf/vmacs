7fe356bff7b4f55bb91c82baf3f3679154ba5e67
jixiuf <jixiuf@qq.com>
2021-01-31 00:05:18 +0800

    vterm

--- conf/conf-vterm.el
+++ conf/conf-vterm.el
@@ -11,6 +11,7 @@
 (setq vterm-toggle-cd-auto-create-buffer t)
 (setq-default vterm-clear-scrollback-when-clearing t)
 (setq-default term-prompt-regexp "^[^#$%>\n]*[#$%>] *") ;默认regex 相当于没定义，term-bol无法正常中转到开头处
+(setq vterm-buffer-name-string "vterm %s")
 
 (require 'vterm)
 (require 'vterm-toggle)
@@ -54,13 +55,10 @@
         (message "vterm-copy-mode enabled")
         (unless (evil-normal-state-p)
           (evil-normal-state)))
-
     (unless (evil-insert-state-p)
       (evil-insert-state))))
 
 (add-hook 'vterm-copy-mode-hook #'vmacs-vterm-copy-mode-hook)
-
-
 (add-hook 'evil-insert-state-entry-hook 'vmacs-vterm-enable-output)
 
 (defun vterm-eob()
@@ -69,53 +67,37 @@
   (skip-chars-backward "\n[:space:]"))
 
 (define-key vterm-mode-map (kbd "s-C-M-u") 'vterm-toggle)
-;; (define-key vterm-mode-map (kbd "s-t")   #'vterm-toggle-cd-show)
 (define-key vterm-mode-map (kbd "C-c C-g")   #'vterm--self-insert)
 (define-key vterm-mode-map (kbd "s-v")   #'vterm-yank)
-(define-key vterm-mode-map [f2]   nil)
-(define-key vterm-mode-map [f3]   nil)
-
-(define-key vterm-mode-map (kbd "C-.")   #'vterm-reset-cursor-point)
-(define-key vterm-copy-mode-map (kbd "C-.")   #'vterm-reset-cursor-point)
 
 ;; C－s 停止滚屏 C-q恢复滚屏
 (define-key vterm-mode-map (kbd "C-s")   #'vterm-copy-mode)
+(define-key vterm-copy-mode-map (kbd "C-s")   #'vterm-copy-mode)
 (define-key vterm-mode-map [(control return)]   #'vterm-toggle-insert-cd)
 
-(define-key vterm-mode-map (kbd "C-q")   #'vterm-copy-mode)
-(define-key vterm-copy-mode-map (kbd "C-s")   #'vterm-copy-mode)
 (define-key vterm-copy-mode-map (kbd "C-c C-c")   #'vterm-send-C-c)
 (define-key vterm-copy-mode-map (kbd "C-c C-c")   #'vterm-send-C-c)
 ;; (define-key vterm-mode-map (kbd "C-l")   #'vterm-clear)
 (define-key vterm-mode-map (kbd "C-c C-e")   #'compilation-shell-minor-mode)
 (define-key vterm-copy-mode-map [remap self-insert-command] #'vterm--self-insert)
 
+(evil-collection-define-key 'insert 'vterm-mode-map
+  (kbd "C-r") 'vmacs-vterm-self-insert
+  (kbd "C-g") 'vterm-ctrl-g)
+(evil-collection-define-key 'normal 'vterm-mode-map
+  (kbd "C-g") 'vterm-ctrl-g
+  (kbd "C-p") 'vmacs-vterm-self-insert
+  (kbd "C-n") 'vmacs-vterm-self-insert
+  (kbd "C-r") 'vmacs-vterm-self-insert
+  (kbd "C-y") 'vterm-yank
+  (kbd "C-/") 'vterm-undo
+  "G" 'vterm-eob)
 
 (defun vmacs-vterm-hook()
   (let ((p (get-buffer-process (current-buffer))))
-    (when p (set-process-query-on-exit-flag p nil)))
-  (evil-local-mode 1)
-  (evil-define-key 'insert 'local [escape] 'vterm--self-insert)
-  (evil-define-key 'motion'local (kbd "C-r") 'vmacs-vterm-self-insert)
-  (evil-define-key 'insert 'local (kbd "C-g") 'vterm-ctrl-g)
-  (evil-define-key 'normal'local (kbd "C-g") 'vterm-ctrl-g)
-  (evil-define-key 'normal 'local (kbd "C-p") 'vmacs-vterm-self-insert)
-  (evil-define-key 'normal 'local (kbd "C-n") 'vmacs-vterm-self-insert)
-  (evil-define-key 'normal 'local (kbd "C-r") 'vmacs-vterm-self-insert)
-  (evil-define-key 'normal 'local (kbd "C-y") 'vterm-yank)
-  (evil-define-key 'normal 'local (kbd "C-/") 'vterm-undo)
-  (evil-define-key 'normal 'local "c" 'vterm-evil-change)
-
-  (evil-define-key 'normal 'local (kbd "G") 'vterm-eob))
+    (when p (set-process-query-on-exit-flag p nil))))
 
 (add-hook 'vterm-mode-hook 'vmacs-vterm-hook)
-;; (add-hook 'vterm-mode-hook  'with-editor-export-editor)
-(setq vterm-buffer-name-string "vterm %s")
-(defun vterm-evil-change ()
-  "Provide similar behavior as `evil-change'."
-  (interactive)
-    (cl-letf (((symbol-function #'delete-region) #'vterm-delete-region))
-      (call-interactively #'evil-change)))
 
 
 
