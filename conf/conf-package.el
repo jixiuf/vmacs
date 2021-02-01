3db1aa4177aa4d01ea812899e7e5241ad95d5513
jixiuf <jixiuf@qq.com>
2021-02-01 11:40:24 +0800

    package

--- conf/conf-package.el
+++ conf/conf-package.el
@@ -1,24 +1,9 @@
-;;确保需要的package 在本地都存在
 (require 'package)
-(setq package-name-column-width 30)
-;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-;; mirror
-  ;; (setq package-archives
-  ;;       '(("melpa-cn" . "http://mirrors.cloud.tencent.com/elpa/melpa/")
-  ;;         ("org-cn"   . "http://mirrors.cloud.tencent.com/elpa/org/")
-  ;;         ("gnu-cn"   . "http://mirrors.cloud.tencent.com/elpa/gnu/")))
   (setq package-archives
         '(("melpa-cn" . "http://mirrors.163.com/elpa/melpa/")
+          ("nognu-cn" . "http://mirrors.163.com/elpa/nongnu/")
           ("gnu-cn"   . "http://mirrors.163.com/elpa/gnu/")))
 
-;; (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-;;                          ("melpa" . "http://elpa.emacs-china.org/melpa/")))
-
-;; (add-to-list 'package-archives '("melpa" . "http://www.mirrorservice.org/sites/melpa.org/packages/"))
-
-;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
-;; make sure to have downloaded archive description.
-;; Or use package-archive-contents as suggested by Nicolas Dudebout
 (or (file-exists-p package-user-dir) (package-refresh-contents))
 ;; (package-initialize)
 
@@ -36,17 +21,9 @@ Return a list of installed packages or nil for every skipped package."
          package)))
    packages))
 
-;; 国内有人做了个melpa 的镜像，
-;; (add-to-list 'package-archives '("popkit" . "http://elpa.popkit.org/packages/"))
 
 (ensure-package-installed package-selected-packages)
 
-;; 为evil-mode 的gg 让位
-(define-key package-menu-mode-map "g" nil)
-(define-key package-menu-mode-map "/" nil)
-;; 为evil-mode 的n让位
-(define-key tabulated-list-mode-map "n" nil)
-(define-key tabulated-list-mode-map "0" nil)
 (provide 'conf-package)
 
 ;; Local Variables:
