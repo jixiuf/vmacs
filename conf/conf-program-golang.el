d6afa0563497d6550178d9a5993e7b56d21836a7
jixiuf <jixiuf@qq.com>
2021-02-01 15:04:46 +0800

    go

--- conf/conf-program-golang.el
+++ conf/conf-program-golang.el
@@ -7,23 +7,18 @@
       (dolist (path  (parse-colon-path gopath))
         (setq exec-path (delete-dups  (cons (concat path  "/bin") exec-path))))))
 
-(when (executable-find "gofmt")
-  (setq-default gofmt-command (executable-find "gofmt")))
-(when (executable-find "goimports")
-  (setq-default gofmt-command (executable-find "goimports")))
+(when (executable-find "gofmt") (setq-default gofmt-command (executable-find "gofmt")))
+(when (executable-find "goimports") (setq-default gofmt-command (executable-find "goimports")))
+
 (add-hook 'go-mode-hook 'vmacs-go-mode-hook)
-
-
 (defun vmacs-go-mode-hook()
   (setq eglot-workspace-configuration
         ;; https://github.com/golang/tools/blob/master/gopls/doc/emacs.md
         '((:gopls . (:usePlaceholders t :completeUnimported  t ;; :staticcheck t
                                       :experimentalWorkspaceModule  t
                                       :allowModfileModifications t))))
-  ;; (add-hook 'before-save-hook #'gofmt 20 t)
-  ;; (add-hook 'before-save-hook #'lsp-organize-imports 20 t)
-  (setq require-final-newline nil)
-  (modify-syntax-entry ?_  "_" (syntax-table)) ;还是让 "_" 作为symbol，还不是word
+  ;; (setq require-final-newline nil)
+  ;; (modify-syntax-entry ?_  "_" (syntax-table)) ;还是让 "_" 作为symbol，还不是word
   (local-set-key (kbd "C-c i") 'go-goto-imports)
   (local-set-key (kbd "C-c g") 'golang-setter-getter))
 
