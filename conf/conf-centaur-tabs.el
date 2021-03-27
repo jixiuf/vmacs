ffac3824922cdf5d72394b0c02eb2a88acb0aa2f
jixiuf <jixiuf@qq.com>
2021-03-27 23:15:10 +0800

    tab

--- conf/conf-centaur-tabs.el
+++ conf/conf-centaur-tabs.el
@@ -6,9 +6,6 @@
 (require 'centaur-tabs)
 (global-set-key  (kbd "s-C-M-k") 'centaur-tabs-backward)
 (global-set-key  (kbd "s-C-M-j") 'centaur-tabs-forward)
-;; (define-key evil-normal-state-map (kbd "gh") 'centaur-tabs-move-current-tab-to-left)
-;; (define-key evil-normal-state-map (kbd "gl") 'centaur-tabs-move-current-tab-to-right)
-;; (vmacs-leader (kbd "e") 'centaur-tabs-build-ivy-source)
 (vmacs-leader (kbd "e") 'centaur-tabs-forward-group)
 
 ;; 只为eshell-mode term-mode 启用centaur-tabs
@@ -79,59 +76,7 @@
 (setq centaur-tabs-label-fixed-length 30)
 (centaur-tabs-mode t)
 
-
-;; (defun vmacs-centaur-tabs-buffer-list ()
-;;   "Return the list of buffers to show in tabs.
-;; only show eshell-mode term-mode and shell-mode."
-;;   (centaur-tabs-filter
-;;    #'vmacs-show-tabbar-p
-;;    (buffer-list)))
-;; (setq centaur-tabs-buffer-list-function 'vmacs-centaur-tabs-buffer-list)
-
-;; (defun vmacs-show-tabbar-p(&optional buf redisplay)
-;;   (let ((show t))
-;;     (with-current-buffer (or buf (current-buffer))
-;;       (cond
-;;        ((char-equal ?\  (aref (buffer-name) 0))
-;;         (setq show nil))
-;;        ((member (buffer-name) '("*Ediff Control Panel*"
-;;                                 "*Completions*"
-;;                                 "*Ido Completions*"
-;;                                 "\*Flycheck error messages\*"
-;;                                 "\*Gofmt Errors\*"))
-;;         (setq show nil))
-;;        (t t))
-;;       (unless show
-;;         ;; (kill-local-variable 'header-line-format)
-;;         (setq header-line-format nil)
-;;         (when redisplay (redisplay t))
-;;         )
-;;       show)))
-
-;; (defun vmacs-hide-tab-p(buf)
-;;   (not (vmacs-show-tabbar-p buf t)))
-
-;; (setq centaur-tabs-hide-tab-function #'vmacs-hide-tab-p)
-
 (add-hook 'after-init-hook (lambda() (remove-hook 'kill-buffer-hook 'centaur-tabs-buffer-track-killed)))
-
-
-;; ;; Copied from s.el
-;; (defadvice centaur-tabs-truncate-string (around vmacs-tab activate)
-;;   "If S is longer than LEN, cut it down and add ELLIPSIS to the end.
-
-;; The resulting string, including ellipsis, will be LEN characters
-;; long.
-
-;; When not specified, ELLIPSIS defaults to ‘...’."
-;;   (declare (pure t) (side-effect-free t))
-;;   (unless ellipsis (setq ellipsis ""))
-;;   (setq ad-return-value
-;;         (if (> (length s) len)
-;;             (format "%s%s" (substring s 0 (- len (length ellipsis))) ellipsis)
-;;           s)))
-
-
 
 (provide 'conf-centaur-tabs)
 
