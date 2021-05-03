3eea3cb84de34a961ea4f59891644ad978855175
jixiuf <jixiuf@qq.com>
2021-05-03 23:47:46 +0800

    mail

--- conf/conf-mail.el
+++ conf/conf-mail.el
@@ -15,23 +15,24 @@
    mu4e-attachment-dir "~/Downloads/mail"
    mu4e-update-interval 300
    mu4e-split-view 'single-window
+   mu4e-completing-read-function #'completing-read
+   ;; this setting allows to re-sync and re-index mail by pressing U
+   mu4e-get-mail-command  "mbsync -a"
+   ;; mu4e-use-fancy-chars t
    )
 
-  ;; this setting allows to re-sync and re-index mail
-  ;; by pressing U
-  (setq mu4e-get-mail-command  "mbsync -a")
-(evil-collection-define-key 'normal 'mu4e-headers-mode-map
-   "," #'mu4e~headers-jump-to-maildir)
+  (evil-collection-define-key 'normal 'mu4e-headers-mode-map
+    "," #'mu4e~headers-jump-to-maildir))
 
-;; (setq
-;;    message-send-mail-function   'smtpmail-send-it
-;;    smtpmail-default-smtp-server "smtp.exmail.qq.com"
-;;    smtpmail-smtp-server         "smtp.exmail.qq.com")
+  ;; (setq
+  ;;    message-send-mail-function   'smtpmail-send-it
+  ;;    smtpmail-default-smtp-server "smtp.exmail.qq.com"
+  ;;    smtpmail-smtp-server         "smtp.exmail.qq.com")
 
-(provide 'conf-mail)
+  (provide 'conf-mail)
 
-;; Local Variables:
-;; coding: utf-8
-;; End:
+  ;; Local Variables:
+  ;; coding: utf-8
+  ;; End:
 
 ;;; conf-mail.el ends here.
