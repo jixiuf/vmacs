bbf6deb3c54404081732c35d6a218d59f6d0a9dc
jixiuf <jixiuf@qq.com>
2021-07-05 20:02:57 +0800

    (setq vmacs-dired-history-max 1000)

--- conf/conf-dired.el
+++ conf/conf-dired.el
@@ -166,7 +166,7 @@
 
 ;; 绑定之后，你访问过的dired都会被记录住，当你copy rename 及打开dired时，可以从这些
 ;; 已访问的目录中筛选以方便快速访问
-(setq vmacs-dired-history-max 500)
+(setq vmacs-dired-history-max 1000)
 (require 'vmacs-dired-history)
 
 (require 'dired-async nil t)
