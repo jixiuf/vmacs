f5c6f1f101249c9e5ec4061185d6c954d99531b1
jixiuf <jixiuf@qq.com>
2022-08-05 19:09:01 +0800

    build emacs

--- bin/build-emacs.sh
+++ bin/build-emacs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 rm -rf ~/Library/Caches/Homebrew/emacs-plus@29--git
 ln -sf ~/repos/emacs ~/Library/Caches/Homebrew/emacs-plus@29--git
+
+# ./configure "CFLAGS=-DFD_SETSIZE=10000 -DDARWIN_UNLIMITED_SELECT"
+export CFLAGS="-DFD_SETSIZE=10000 -DDARWIN_UNLIMITED_SELECT"
+# (shell-command-to-string "ulimit -n")
 brew reinstall --with-native-comp --with-xwidgets emacs-plus@29
