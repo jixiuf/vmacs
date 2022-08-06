b317ba9d4c9ca997aa9d17123ce488f7b0a8ed9a
jixiuf <jixiuf@qq.com>
2022-08-06 11:44:54 +0800

    path build emacs

--- bin/build-emacs.sh
+++ bin/build-emacs.sh
@@ -2,7 +2,10 @@
 rm -rf ~/Library/Caches/Homebrew/emacs-plus@29--git
 ln -sf ~/repos/emacs ~/Library/Caches/Homebrew/emacs-plus@29--git
 
-# ./configure "CFLAGS=-DFD_SETSIZE=10000 -DDARWIN_UNLIMITED_SELECT"
-export CFLAGS="-DFD_SETSIZE=10000 -DDARWIN_UNLIMITED_SELECT"
-# (shell-command-to-string "ulimit -n")
+# cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/ ;git diff >~/.emacs.d/bin/emacs.patch
+cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/
+patch -t -N -p1 <~/.emacs.d/bin/emacs.patch
+cd -
+
 brew reinstall --with-native-comp --with-xwidgets emacs-plus@29
+cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/;git stash ;git stash drop
