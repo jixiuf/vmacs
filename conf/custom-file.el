280e47134ad4dcbc277bf35156da4e1676124dd8
jixiuf <jixiuf@qq.com>
2022-08-20 10:19:46 +0800

    mu4e-related-face

--- conf/custom-file.el
+++ conf/custom-file.el
@@ -6,7 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mu4e-related-face ((t (:inherit default :foreground "#bfebe0" :slant italic)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
