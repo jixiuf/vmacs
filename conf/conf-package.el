0b37e9407a9faedc6bb2707b173f3ef0d20faa34
jixiuf <jixiuf@qq.com>
2022-11-09 21:45:55 +0800

    pkg install

--- conf/conf-package.el
+++ conf/conf-package.el
@@ -16,9 +16,8 @@ Return a list of installed packages or nil for every skipped package."
      ;; (package-installed-p 'evil)
      (if (package-installed-p package)
          nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
            (package-install package)
-         package)))
+         package))
    packages))
 
 
