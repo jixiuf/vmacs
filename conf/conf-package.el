5451581b63229d397e5a0e7b35f78f5c4bb09de2
jixiuf <jixiuf@qq.com>
2022-11-19 22:21:23 +0800

    update conf-pkg

--- conf/conf-package.el
+++ conf/conf-package.el
@@ -6,22 +6,8 @@
 
 (or (file-exists-p package-user-dir) (package-refresh-contents))
 ;; (package-initialize)
-
-(defun ensure-package-installed (packages)
-  "Assure every package is installed, ask for installation if it’s not.
-
-Return a list of installed packages or nil for every skipped package."
-  (mapcar
-   (lambda (package)
-     ;; (package-installed-p 'evil)
-     (if (package-installed-p package)
-         nil
-           (package-install package)
-         package))
-   packages))
-
-
-(add-hook 'after-init-hook (lambda() (ensure-package-installed package-selected-packages)))
+(add-hook 'after-init-hook (lambda() (package-install-selected-packages t)
+                             (package-vc-install-selected-packages)))
 
 (provide 'conf-package)
 
