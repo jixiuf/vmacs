d9dadfcae32633066b568f58560f8d285d965313
jixiuf <jixiuf@qq.com>
2022-12-26 14:36:09 +0800

    fix build-emacs.sh

--- bin/build-emacs.sh
+++ bin/build-emacs.sh
@@ -3,9 +3,9 @@ rm -rf ~/Library/Caches/Homebrew/emacs-plus@29--git
 ln -sf ~/repos/emacs ~/Library/Caches/Homebrew/emacs-plus@29--git
 
 # cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/ ;git diff >~/.emacs.d/bin/emacs.patch
-cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/
-patch -t -N -p1 <~/.emacs.d/bin/emacs.patch
-cd -
+# cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/
+# patch -t -N -p1 <~/.emacs.d/bin/emacs.patch
+# cd -
 
 brew reinstall --with-native-comp --with-xwidgets --with-poll emacs-plus@29
-cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/;git stash ;git stash drop
+# cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/;git stash ;git stash drop
