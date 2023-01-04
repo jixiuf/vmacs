d3f254c27b1f55a540f3d7033149a6311e396fde
jixiuf <jixiuf@qq.com>
2023-01-04 10:35:54 +0800

    emacs 30

--- bin/build-emacs.sh
+++ bin/build-emacs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-rm -rf ~/Library/Caches/Homebrew/emacs-plus@29--git
-ln -sf ~/repos/emacs ~/Library/Caches/Homebrew/emacs-plus@29--git
+rm -rf ~/Library/Caches/Homebrew/emacs-plus@30--git
+ln -sf ~/repos/emacs ~/Library/Caches/Homebrew/emacs-plus@30--git
 
 # cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/ ;git diff >~/.emacs.d/bin/emacs.patch
 # cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/
 # patch -t -N -p1 <~/.emacs.d/bin/emacs.patch
 # cd -
 
-brew reinstall --with-native-comp --with-xwidgets --with-poll emacs-plus@29
+brew reinstall --with-native-comp --with-xwidgets --with-poll emacs-plus@30
 # cd /usr/local/Homebrew/Library/Taps/d12frosted/homebrew-emacs-plus/;git stash ;git stash drop
