2d5b8ae598bdfddfe747b333795d2c0b9592a1ef
jixiuf <jixiuf@qq.com>
2023-11-25 22:29:38 +0800

    delve

--- conf/conf-program-golang.el
+++ conf/conf-program-golang.el
@@ -30,50 +30,31 @@
              )))
 
   (setq dape-buffers-on-start  '(dape-info))
-
-  (defun vmacs-dape--select-go-args ()
-    (require 'which-func)
-    (if (string-suffix-p "_test.go"   (buffer-name))
-        (when-let* ((test-name (which-function))
-                    (test-regexp (concat "^" test-name "$")))
-          (if test-name
-              `["-test.run" ,test-regexp]
-            (error "No test selected")))
-      (if  current-prefix-arg
-          (vconcat (split-string (read-shell-command "args: " nil
-                                                     (if (equal (car compile-history) "")
-                                                         '(compile-history . 1)
-                                                       'compile-history))))
-        [])))
-
-  ;; https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
-  (defun vmacs-dape-test-p ()
-    (if (string-suffix-p "_test.go"   (buffer-name))
-        "test" "debug"))
-
-  (defun vmacs-dape-relative-dir ()
-    "Return the file directory relative to dape's cwd. This is used by Delve debugger."
-    (if (string-suffix-p "_test.go"   (buffer-name))
-        (concat "./" (file-relative-name
-                      default-directory (funcall dape-cwd-fn)))
-      (funcall dape-cwd-fn)))
-
   ;; inside your dape-config
   (add-to-list 'dape-configs
                `(delve
                  modes (go-mode go-ts-mode)
+                 ensure dape-ensure-command
+                 fn dape-config-autoport
                  command "dlv"
+                 command-args ("dap" "--listen" "127.0.0.1::autoport")
                  command-cwd dape-cwd-fn
-                 command-args ("dap" "--listen" "127.0.0.1:55878")
-                 host "127.0.0.1"
-                 port 55878
-                 :type "go"
-                 :name "go-debug"
+                 port :autoport
+                 :type "debug"
                  :request "launch"
-                 :mode vmacs-dape-test-p
+                 :mode (lambda() (if (string-suffix-p "_test.go"   (buffer-name)) "test" "debug"))
                  :cwd dape-cwd-fn
-                 :program vmacs-dape-relative-dir
-                 :args vmacs-dape--select-go-args))
+                 :program (lambda()(if (string-suffix-p "_test.go"   (buffer-name))
+                                       (concat "./" (file-relative-name default-directory (funcall dape-cwd-fn)))
+                                     (funcall dape-cwd-fn)))
+                 :args (lambda()
+                         (require 'which-func)
+                         (if (string-suffix-p "_test.go"   (buffer-name))
+                             (when-let* ((test-name (which-function))
+                                         (test-regexp (concat "^" test-name "$")))
+                               (if test-name `["-test.run" ,test-regexp]
+                                 (error "No test selected")))
+                           []))))
   )
 
 (defun vmacs-go-mode-hook()
