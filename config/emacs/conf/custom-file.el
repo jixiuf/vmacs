c986d17665aebaa28db4f24ce2abb22527df887c
jixiuf <jixiuf@qq.com>
2024-04-29 10:57:30 +0800

    up

--- config/emacs/conf/custom-file.el
+++ config/emacs/conf/custom-file.el
@@ -51,10 +51,10 @@
  '(modus-themes-scale-headings t)
  '(modus-themes-scale-title 1.5)
  '(package-selected-packages
-   '(async cape clipetty consult-dir corfu corfu-terminal dired-filetype-face eglot emamux embark
-           embark-consult exec-path-from-shell fanyi git-link golden-ratio-scroll-screen goto-chg
-           kind-icon lua-mode magit marginalia markdown-mode mu4e-alert orderless org-ai org-msg
-           ox-gfm pinyinlib protobuf-mode verb vundo wgrep with-editor yasnippet))
+   '(async beacon cape clipetty consult-dir corfu corfu-terminal dired-filetype-face eglot emamux
+           embark embark-consult exec-path-from-shell fanyi git-link golden-ratio-scroll-screen
+           goto-chg kind-icon lua-mode magit marginalia markdown-mode mu4e-alert orderless org-ai
+           org-msg ox-gfm pinyinlib protobuf-mode verb vundo wgrep with-editor yasnippet))
  '(proced-enable-color-flag t)
  '(proced-format 'long)
  '(recentf-save-file "~/.cache/emacs/recentf")
