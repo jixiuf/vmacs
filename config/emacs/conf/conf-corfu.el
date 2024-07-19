484a91a925a49c656d8c7c539442a9d562714596
jixiuf <jixiuf@qq.com>
2024-07-19 17:46:55 +0800

    up

--- config/emacs/conf/conf-corfu.el
+++ config/emacs/conf/conf-corfu.el
@@ -49,7 +49,7 @@
 (add-to-list 'completion-at-point-functions #'cape-file)
 (add-to-list 'completion-at-point-functions #'cape-dabbrev)
 
-(global-set-key (kbd "C-<return>") (cape-capf-interactive #'codeium-completion-at-point))
+;; (global-set-key (kbd "C-<return>") (cape-capf-interactive #'codeium-completion-at-point))
 
 
 (provide 'conf-corfu)
