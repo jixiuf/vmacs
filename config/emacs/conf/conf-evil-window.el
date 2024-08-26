70e903527637a4c0a368bb91a00c6e7d60fb4929
jixiuf <jixiuf@qq.com>
2024-08-26 16:19:20 +0800

    vmacs-fullscreen

--- config/emacs/conf/conf-evil-window.el
+++ config/emacs/conf/conf-evil-window.el
@@ -38,14 +38,21 @@
       (progn
         (setq vmacs--fullscreen-window-configuration (current-window-configuration))
         (vmacs-delete-other-frame)
-        (when (string-equal "0" (string-trim
-                                 (shell-command-to-string
-                                  "hyprctl activewindow -j| jq -rc '.fullscreen'")))
+        (when (and (string-equal "0" (string-trim
+                                      (shell-command-to-string
+                                       "hyprctl activewindow -j| jq -rc '.fullscreen'")))
+                   (not (string-equal "1" (string-trim
+                                           (shell-command-to-string
+                                            "hyprctl clients -j | jq -cr '.[] | select(.workspace.id == '$(hyprctl activeworkspace -j|jq -rc \".id\")')|.address'|wc -l")))
+                        (call-process "hyprctl" nil nil nil "dispatch" "fullscreen" "1")))
           (call-process "hyprctl" nil nil nil "dispatch" "fullscreen" "1")))
     (when vmacs--fullscreen-window-configuration
       (set-window-configuration vmacs--fullscreen-window-configuration)
       (setq vmacs--fullscreen-window-configuration nil))
-    (call-process "hyprctl" nil nil nil "dispatch" "fullscreen" "1")))
+    (unless (string-equal "1" (string-trim
+                               (shell-command-to-string
+                                "hyprctl clients -j | jq -cr '.[] | select(.workspace.id == '$(hyprctl activeworkspace -j|jq -rc \".id\")')|.address'|wc -l")))
+      (call-process "hyprctl" nil nil nil "dispatch" "fullscreen" "1"))))
 
 (global-set-key (kbd "C-s-o") 'vmacs-other-window)
 (global-set-key (kbd "C-x o") 'vmacs-other-window)
