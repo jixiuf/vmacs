5fce604cf00ecad30a45e727b33fa6cc7d8c2934
jixiuf <jixiuf@qq.com>
2024-11-18 11:12:51 +0800

    up

--- config/emacs/conf/conf-icomplete.el
+++ config/emacs/conf/conf-icomplete.el
@@ -93,7 +93,7 @@ Monospaced font whihc is fixed idth and height is recommended."
 (define-key completion-preview-active-mode-map (kbd "C-n") #'vmacs-complete)
 (define-key completion-preview-active-mode-map (kbd "C-i") #'vmacs-complete)
 (define-key completion-preview-active-mode-map (kbd "C-o") #'completion-preview-complete)
-(define-key completion-preview-active-mode-map (kbd "C-m") #'completion-preview-insert)
+(define-key completion-preview-active-mode-map (kbd "C-j") #'completion-preview-insert)
 (define-key completion-preview-active-mode-map (kbd "C-s") #'completion-preview-next-candidate)
 (define-key completion-preview-active-mode-map (kbd "M-f") #'completion-preview-insert-word)
 (setq completion-preview-minimum-symbol-length nil)
