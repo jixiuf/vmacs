1ed858fa914b8f3cb8df59217e11450bc3a13f38
jixiuf <jixiuf@qq.com>
2024-12-11 19:18:52 +0800

    mail

--- config/emacs/gnus.el
+++ config/emacs/gnus.el
@@ -1,4 +1,5 @@
 ;; https://forums.freebsd.org/threads/do-you-use-emacs-gnus.41969/
+;; https://ericabrahamsen.net/tech/2014/oct/gnus-dovecot-lucene.html
 (setq message-send-mail-function 'smtpmail-send-it)
 (when (member (system-name) '("jxfhome" "jxfluoji"))
   (load  (concat user-emacs-directory "conf/conf-private.el.gpg") t))
@@ -14,29 +15,46 @@
 
 ;;; sending mail end
 
+;; 这些配置是因我是meow 用户，我对"g" "G" 做了一些定制
+(with-eval-after-load 'gnus-group
+  (define-key gnus-group-mode-map (kbd "C-c MG") gnus-group-group-map)     ;old gnus G
+  (define-key gnus-group-mode-map (kbd "C-c Gr") #'gnus-group-get-new-news))     ;old gnus g, now gr
+(with-eval-after-load 'gnus-summary
+  (define-key gnus-summary-mode-map (kbd "C-c MG") gnus-summary-goto-map)     ;old gnus G
+  (define-key gnus-summary-mode-map (kbd "C-c Gr") #'gnus-summary-show-article)) ;old gnus g ,now gr
 
 (setq gnus-select-method '(nnnil ""))
 
 (setq gnus-secondary-select-methods
       `(
-        ;; (nnmaildir "jixiuf"
-        ;;           (directory "~/maildir/qq")
-        ;;           )
-        ;; (nnmaildir ""
-        ;;            (directory "~/maildir/qq/&UXZO1mWHTvZZOQ-/")
-        ;;           )
+        (nnmaildir "jixiuf"
+                  (directory "~/maildir/qq")
+                  )
+        (nnmaildir ""
+                   (directory "~/maildir/qq/&UXZO1mWHTvZZOQ-/")
+                  )
         ;; (nnml "vmacs"
         ;;           (directory "~/maildir/vmacs")
         ;;           )
-        (nnimap ,user-full-name
-                (nnimap-address "imap.qq.com")
-                (nnimap-inbox "INBOX")
-                (nnimap-expunge t)
-                (nnimap-server-port 993)
-                (nnimap-stream ssl))
+        ;; (nnimap ,user-mail-address
+        ;;         (nnimap-address "localhost")
+        ;;         ;; (nnimap-inbox "inbox")
+        ;;         (nnimap-expunge t)
+        ;;         ;; (nnimap-user ,user-full-name)
+        ;;         (nnimap-server-port 143)
+        ;;         (nnimap-stream network))
+        ;; (nnimap ,user-full-name
+        ;;         (nnimap-address "imap.qq.com")
+        ;;         (nnimap-inbox "INBOX")
+        ;;         (nnimap-expunge t)
+        ;;         (nnimap-server-port 993)
+        ;;         (nnimap-stream ssl))
         ))
-(setq mail-sources
-      '((maildir :path "~/maildir/")))
+(setq gnus-parameters
+  '(("nnmaildir.*"
+     (gnus-use-scoring nil)
+     ;; (expiry-wait . 2)
+     (display . all))))
 (setq mm-discouraged-alternatives '("text/html" "text/richtext"))
 (when window-system
   (setq gnus-sum-thread-tree-indent "  ")
@@ -50,7 +68,8 @@
 (setq gnus-summary-line-format
       (concat
        "%0{%U%R%z%}"
-       "%3{│%}" "%1{%&user-date; %}" "%3{│%}" ;; date
+       ;; "%3{│%}" "%1{%d%}" "%3{│%}" ;; date
+       "%3{│%}" "%3{%-18,18&user-date; %}" "%3{│%}" ;; date
        "  "
        "%4{%-20,20f%}"               ;; name
        "  "
@@ -59,15 +78,20 @@
        "%1{%B%}"
        "%s\n"))
 ;; (setq gnus-activate-level 4)
+(setq gnus-use-dribble-file nil)
+(setq gnus-always-read-dribble-file nil)
+(setq gnus-save-newsrc-file nil)
+(setq gnus-read-newsrc-file nil)
+(setq gnus-fetch-old-headers t)
 (setq gnus-article-date-headers (quote (local)))
 (setq gnus-summary-mode-hook 'hl-line-mode)
 (setq gnus-group-mode-hook (quote (gnus-topic-mode hl-line-mode)))
 ;; gnus-summary-line-format 内通过%&user-date; 自定义时间格式
 (setq gnus-user-date-format-alist (quote (
-					  ((gnus-seconds-today) . "%H:%M")
-					  ((+ 86400 (gnus-seconds-today)) . "昨%H:%M")
-					  ((gnus-seconds-year) . "%a %b %d %H:%M")
-					  (t . "%a %b %d %Y %H:%M"))))
+					  ((gnus-seconds-today) . "%a %b%d %H:%M 今")
+					  ((+ 86400 (gnus-seconds-today)) . "%a %b%d %H:%M 昨")
+					  ((gnus-seconds-year) . "%a %b%d %H:%M")
+					  (t . "%a %Y%b%d %H:%M"))))
 ;; (setq gnus-summary-line-format "%U%R%([%-30,30f]:%) %-50,40s(%&user-date;)\n")
 ;; set gnus-parameter
 
@@ -80,11 +104,7 @@
 ;;                          ))
 ;; (gnus-topic-set-parameters "vmacs" '((display . 200)))
 ;; (gnus-topic-set-parameters "jixiuf" '((display . 200))))
-(setq gnus-parameters
-  '(("nnimap.*"
-     (gnus-use-scoring nil)
-     (expiry-wait . 2)
-     (display . all))))
+
 
 ;; https://www.bounga.org/tips/2020/05/03/multiple-smtp-accounts-in-gnus-without-external-tools/
 (setq gnus-posting-styles
