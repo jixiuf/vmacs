fe3abba6210fca76f67f63760822f46a26f7ca70
jixiuf <jixiuf@qq.com>
2024-12-12 23:33:36 +0800

    update

--- config/emacs/gnus.el
+++ config/emacs/gnus.el
@@ -1,19 +1,24 @@
 ;; https://forums.freebsd.org/threads/do-you-use-emacs-gnus.41969/
 ;; https://ericabrahamsen.net/tech/2014/oct/gnus-dovecot-lucene.html
-(setq message-send-mail-function 'smtpmail-send-it)
 (when (member (system-name) '("jxfhome" "jxfluoji"))
   (load  (concat user-emacs-directory "conf/conf-private.el.gpg") t))
 
 ;; C-c C-m f – Attach file
 ;; C-c C-d – Save message as draft
-(setq smtpmail-smtp-user user-mail-address
+;; C-c C-b: goto body
+;; C-c C-f C-t: goto To:
+;; C-c C-f C-o: goto From:
+;; C-c C-f C-c: goto Cc:
+;; C-c C-c :send
+;; C-c C-k : 丢弃
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-smtp-user user-mail-address
       smtpmail-smtp-server "smtp.qq.com"
       smtpmail-smtp-service 465
-      smtpmail-stream-type 'ssl)
+      smtpmail-stream-type 'ssl
+      smtpmail-debug-info t
+      smtpmail-debug-verb t)
 
-;;Debug
-(setq smtpmail-debug-info t)
-(setq smtpmail-debug-verb t)
 ;; https://www.bounga.org/tips/2020/05/03/multiple-smtp-accounts-in-gnus-without-external-tools/
 (setq gnus-posting-styles
       `((".*" ; Matches all groups of messages with default qq
@@ -23,28 +28,50 @@
          ("X-Message-SMTP-Method" (concat "smtp smtp.qq.com 587 " ,user-mail-address-2)))))
 
 ;; (setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+(with-eval-after-load 'message
+  ;; From: 后 tab 键 切换 发送邮箱
+  (add-to-list 'message-completion-alist (cons "^\\([^ :]*-\\)?\\(From\\):" #'message-toggle-from)))
 
+;; user-work-mail-address user-mail-address-2 等变量为我其他两个邮箱地址
+(defun message-toggle-from()
+  "Toggle addresses in the From: field of the message buffer."
+  (interactive)
+  (save-excursion
+    (message-goto-from)
+    (cond
+     ((string-match (concat "^from:\s-*.*" user-mail-address) (thing-at-point 'line))
+	  (beginning-of-line)
+	  (message-delete-line)
+      (insert (concat "From: " user-full-name " <" user-work-mail-address ">\n")))
+     ((string-match (concat "^from:\s-*.*" user-work-mail-address) (thing-at-point 'line))
+	  (beginning-of-line)
+	  (message-delete-line)
+      (insert (concat "From: " user-mail-address-2 "\n")))
+     (t
+	  (beginning-of-line)
+	  (message-delete-line)
+	  (insert (concat "From: " user-full-name " <" user-mail-address ">\n"))))))
 
-;;; sending mail end
+;;; sending mail end here.
 
-;; 这些配置是因我是meow 用户，我对"g" "G" 做了一些定制
 (with-eval-after-load 'gnus-group
-  (define-key gnus-group-mode-map (kbd "C-c MG") gnus-group-group-map)     ;old gnus G
-  (define-key gnus-group-mode-map (kbd "C-c Gr") #'gnus-group-get-new-news))     ;old gnus g, now gr
+;; 这些配置是因我是meow 用户，我对"g" "G" 做了一些定制
+  (define-key gnus-group-mode-map (kbd "C-c MG") gnus-group-group-map)     ;old  G
+  (define-key gnus-group-mode-map (kbd "C-c Gr") #'gnus-group-get-new-news))     ;old  g, now gr
 (with-eval-after-load 'gnus-sum
-  (define-key gnus-summary-mode-map (kbd "t") #'gnus-summary-thread-map)     ;old T
-  (define-key gnus-summary-mode-map (kbd "T") #'gnus-summary-toggle-header)     ;old t
+  ;; 交换t T 后 tk:标记当前thread分支为已读
+  (define-key gnus-summary-mode-map  "t" #'gnus-summary-thread-map)     ;old T
+  (define-key gnus-summary-mode-map  "T" #'gnus-summary-toggle-header)     ;old t
+  (define-key gnus-summary-thread-map "t" #'gnus-summary-toggle-threads)   ;tt :切换是否thread old:TT
 
-  (define-key gnus-summary-mode-map (kbd "v") #'gnus-summary-next-page)     ;old space
+  (define-key gnus-summary-mode-map  "v" #'gnus-summary-next-page)     ;old space
   (define-key gnus-summary-mode-map (kbd "C-c MG") gnus-summary-goto-map)     ;old gnus G
   (define-key gnus-summary-mode-map (kbd "C-c Gr") #'gnus-summary-show-article)) ;old gnus g ,now gr
 
-(setq gnus-select-method '(nnnil ""))
-
+(setq gnus-select-method `(nnmaildir ,user-full-name (directory "~/maildir/qq"))) ;(nnnil "")
 (setq gnus-secondary-select-methods
-      `((nnmaildir "jixiuf" (directory "~/maildir/qq"))
+      `((nnmaildir "vmacs"  (directory "~/maildir/vmacs"))
         ;; (nntp "news.gmane.org")
-        (nnmaildir "vmacs"  (directory "~/maildir/vmacs"))
         ;; (nnimap ,user-mail-address
         ;;         (nnimap-address "localhost")
         ;;         ;; (nnimap-inbox "inbox")
@@ -64,7 +91,7 @@
      (gnus-use-scoring nil)
      ;; (expiry-wait . 2)
      (display . all))))
-(setq mm-discouraged-alternatives '("text/html" "text/richtext"))
+;; (setq mm-discouraged-alternatives '( "text/html" "text/richtext"))
 (when window-system
   (setq gnus-sum-thread-tree-indent " ")
   (setq gnus-sum-thread-tree-root "") ;; "● ")
@@ -117,21 +144,6 @@
 ;; (gnus-topic-set-parameters "jixiuf" '((display . 200))))
 
 
-(defun toggle-from()
-  "Toggle addresses in the From: field of the message buffer."
-  (interactive)
-  (save-excursion
-    (message-goto-from)
-    (if (string-match (concat "^From:\s-*.*" user-work-mail-address) (thing-at-point 'line))
-	(progn 
-	  (beginning-of-line)
-	  (message-delete-line)
-	  (insert (concat "From: " user-full-name " <" user-mail-address ">\n")))
-      (message-goto-from)
-      (when (string-match (concat "^from:\s-*.*" user-mail-address) (thing-at-point 'line))
-	(beginning-of-line)
-	(message-delete-line)
-    (insert (concat "From: " user-full-name " <" user-work-mail-address ">\n"))))))
 ;; (setq gnus-summary-highlight)
 
 
