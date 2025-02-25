6c7908680288367a136fe20ea963c444a29119ec
jixiuf <jixiuf@qq.com>
2025-02-25 17:11:53 +0800

    tressit

--- config/emacs/conf/conf-package.el
+++ config/emacs/conf/conf-package.el
@@ -14,12 +14,12 @@
 (setq treesit-language-source-alist
       '((go . ("https://github.com/tree-sitter/tree-sitter-go.git" "v0.23.4"))
 	    (gomod . ("https://github.com/camdencheek/tree-sitter-go-mod.git" "v1.1.0"))
-        (java . ("https://github.com/tree-sitter/tree-sitter-java.git" "v0.20.2"))
-	    (rust . ("https://github.com/tree-sitter/tree-sitter-rust.git" "v0.23.0"))
+        (java . ("https://github.com/tree-sitter/tree-sitter-java.git" "v0.23.5"))
+	    (rust . ("https://github.com/tree-sitter/tree-sitter-rust.git" "v0.23.2"))
 	    (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile.git" "v0.2.0"))
 	    (yaml . ("https://github.com/ikatyang/tree-sitter-yaml.git" "v0.5.0"))
-        (json . ("https://github.com/tree-sitter/tree-sitter-json.git" "v0.23.0"))
-	    (c . ("https://github.com/tree-sitter/tree-sitter-c.git" "v0.23.0"))
+        (json . ("https://github.com/tree-sitter/tree-sitter-json.git" "v0.24.8"))
+	    (c . ("https://github.com/tree-sitter/tree-sitter-c.git" "v0.23.5"))
 	    (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp.git" "v0.23.0"))))
 
 ;; Install missing grammars.
