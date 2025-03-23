cb4533a80ba894a8cb196fc79a0d02a886bb3042
jixiuf <jixiuf@qq.com>
2025-03-23 22:14:48 +0800

    up

--- config/emacs/conf/conf-keybind.el
+++ config/emacs/conf/conf-keybind.el
@@ -214,7 +214,8 @@
   (when (active-minibuffer-window)
     (select-window (active-minibuffer-window))
     (minibuffer-keyboard-quit))
-  (when (equal last-command 'keyboard-quit)
+  (when (member last-command
+                '(keyboard-quit vmacs-cancel-selection))
     (bury-boring-windows)))
 
 (advice-add 'keyboard-quit :before #'vmacs-bury-boring-windows)
