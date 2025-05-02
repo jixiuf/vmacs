30b725e10477b047b4c2a6d6c3c3de4f6de0717a
jixiuf <jixiuf@qq.com>
2025-05-02 21:06:07 +0800

    long_word_filter

--- mac/rime_input_method/lua/long_word_filter.lua
+++ mac/rime_input_method/lua/long_word_filter.lua
@@ -2,77 +2,45 @@
 -- 感谢&参考于： https://github.com/tumuyan/rime-melt
 -- 不提升包含英文、数字的候选项
 -- 不提升包含 emoji、假名的候选项（通过将此 Lua 放到 simplifier@emoji 前面来实现）
-
-local M = {}
-
-function M.init(env)
+local function long_word_filter(input, env)
     -- 提升 count 个词语，插入到第 idx 个位置，默认 2、4。
-    local config = env.engine.schema.config
     env.name_space = env.name_space:gsub("^*", "")
-    M.count = config:get_int(env.name_space .. "/count") or 2
-    M.idx = config:get_int(env.name_space .. "/idx") or 4
-    M.input_str = env.engine.context.input
-end
+    local config = env.engine.schema.config
+    local count = config:get_int(env.name_space .. "/count") or 2
+    local idx = config:get_int(env.name_space .. "/idx") or 4
 
-function M.func(input)
-    local preedit_len = 0
     local l = {}
     local firstWordLength = 0 -- 记录第一个候选词的长度，提前的候选词至少要比第一个候选词长
-    local done = 0         -- 记录筛选了多少个词条(只提升 count 个词的权重)
+    -- local s1 = 0 -- 记录筛选了多少个英语词条(只提升 count 个词的权重，并且对 comment 长度过长的候选进行过滤)
+    local s2 = 0 -- 记录筛选了多少个汉语词条(只提升 count 个词的权重)
     local i = 1
     for cand in input:iter() do
-        local leng = utf8.len(cand.text)
-        -- 只以第一个候选项的长度作为参考
-        if firstWordLength < 1 then
-            firstWordLength = leng
-        end
-        if preedit_len < 1 then
-            local str = cand.preedit
-            -- 去掉preedit的全拼补全
-            local pos = string.find(str, " [", 1, true)
-            if pos then
-                str = string.sub(str, 1, pos - 1)
-            end
-
-            str = str:gsub("%s+", "")
-            preedit_len = utf8.len(str)
-        end
-        
-        -- 不处理 M.idx 之前的候选项
-        if i < M.idx then
+        leng = utf8.len(cand.text)
+        if (firstWordLength < 1 or i < idx) then
             i = i + 1
+            firstWordLength = leng
             yield(cand)
-        -- 长词直接 yield，其余的放到 l 里
-        elseif leng <= firstWordLength or cand.text:find("[%a%d]") then
-            table.insert(l, cand)
-        -- 如果
-        elseif firstWordLength == 1 and preedit_len == 4 then
+		-- 不知道这两行是干嘛用的，似乎注释掉也没有影响。
+		-- elseif #table > 30 then
+		--     table.insert(l, cand)
+		-- 注释掉了英文的
+		-- elseif ((leng > firstWordLength) and (s1 < 2)) and (string.find(cand.text, "^[%w%p%s]+$")) then
+		--     s1 = s1 + 1
+		--     if (string.len(cand.text) / string.len(cand.comment) > 1.5) then
+		--         yield(cand)
+		--     end
+		-- 换了个正则，否则中英混输的也会被提升
+		-- elseif ((leng > firstWordLength) and (s2 < count)) and (string.find(cand.text, "^[%w%p%s]+$")==nil) then
+        elseif ((leng > firstWordLength) and (s2 < count)) and (string.find(cand.text, "[%w%p%s]+") == nil) then
             yield(cand)
-            done = done + 1
-        elseif firstWordLength == 2 and preedit_len == 6 then
-            yield(cand)
-            done = done + 1
-        elseif firstWordLength <=3 and preedit_len == 8 then
-            yield(cand)
-            done = done + 1
-        elseif firstWordLength <=4 and preedit_len == 10 then
-            yield(cand)
-            done = done + 1
+            s2 = s2 + 1
         else
             table.insert(l, cand)
         end
-        -- 找齐了或者 l 太大了，就不找了，一般前 50 个就够了
-        if done == M.count or #l > 20 then
-            break
-        end
     end
-    -- yield l 及后续的候选项
-    for _, cand in ipairs(l) do
-        yield(cand)
-    end
-    for cand in input:iter() do
+    for i, cand in ipairs(l) do
         yield(cand)
     end
 end
 
-return M
+return long_word_filter
