49e77422c2785629122ca4c4e1d8a277d3db9639
jixiuf <jixiuf@qq.com>
2025-12-30 18:01:30 +0800

    term

--- linux/bin/term.sh
+++ linux/bin/term.sh
@@ -38,9 +38,10 @@ other_args=""
 term_args=""
 tmux_session=""
 title=""
+termenv=""
 
 PROG=$( basename "$0" )
-TEMP=$( getopt --options h --longoptions title:,class:,working-directory:,tmux-session:,help -- "$@" ) || exit 1
+TEMP=$( getopt --options h --longoptions termenv:,title:,class:,working-directory:,tmux-session:,help -- "$@" ) || exit 1
 eval set -- "$TEMP"
 
 for i in "$@"; do
@@ -51,6 +52,11 @@ for i in "$@"; do
             ;;
         --title*)
             title="$2"
+            shift
+            shift
+            ;;
+        --termenv*)
+            termenv="TERM=$2 "
             shift
             shift
             ;;
@@ -88,34 +94,34 @@ regex="(\/ssh:)?([a-zA-Z0-9_\-]+@)?([a-zA-Z0-9_\.\-]+):(.+)"
 regex2="(\/ssh:)?([a-zA-Z0-9_]+@)?([a-zA-Z0-9_\.\-]+)#([0-9]+):(.+)"
 
 if [[ $working_directory =~ $regex ]]; then
-  userat=${BASH_REMATCH[2]}
-  host=${BASH_REMATCH[3]}
-  path=${BASH_REMATCH[4]}
-  if [ -z "$other_args" ]; then
-      cmd="ssh -t $userat$host \"cd $path && exec "'\$SHELL'"\" && exec $SHELL"
-  else
-      cmd="ssh -t $userat$host \"cd $path &&  $other_args; \$SHELL -i -l\" "
-  fi
-  if [ -z "$tmux_session" ]; then
-      eval $TERMINAL $term_args $TERMINAL_EXEC  $SHELL  -i -c \'$cmd\'
-  else
-      $TERMINAL $term_args $TERMINAL_EXEC tmux.sh --session $tmux_session --cwd "$working_directory" "--" $SHELL -i -c \'$cmd\'
-  fi
+    userat=${BASH_REMATCH[2]}
+    host=${BASH_REMATCH[3]}
+    path=${BASH_REMATCH[4]}
+    if [ -z "$other_args" ]; then
+        cmd="$termenv ssh -t $userat$host \"cd $path && exec "'\$SHELL'"\" && exec $SHELL"
+    else
+        cmd="$termenv ssh -t $userat$host \"cd $path &&  $other_args; \$SHELL -i -l\" "
+    fi
+    if [ -z "$tmux_session" ]; then
+        eval $TERMINAL $term_args $TERMINAL_EXEC  $SHELL  -i -c \'$cmd\'
+    else
+        $TERMINAL $term_args $TERMINAL_EXEC tmux.sh --session $tmux_session --cwd "$working_directory" "--" $SHELL -i -c \'$cmd\'
+    fi
 elif [[ $working_directory =~ $regex2 ]]; then
-  userat=${BASH_REMATCH[2]}
-  host=${BASH_REMATCH[3]}
-  port=${BASH_REMATCH[4]}
-  path=${BASH_REMATCH[5]}
-  if [ -z "$other_args" ]; then
-      cmd="ssh -t $userat$host -p $port \"cd $path && exec "'\$SHELL'"\" && exec $SHELL"
-  else
-      cmd="ssh -t $userat$host -p $port \"cd $path &&  $other_args; \$SHELL -i -l\" "
-  fi
-  if [ -z "$tmux_session" ]; then
-      eval $TERMINAL $term_args $TERMINAL_EXEC  $SHELL  -i -c \'$cmd\'
-  else
-      $TERMINAL $term_args $TERMINAL_EXEC tmux.sh --session $tmux_session --cwd "$working_directory" "--" $SHELL -i -c \'$cmd\'
-  fi
+    userat=${BASH_REMATCH[2]}
+    host=${BASH_REMATCH[3]}
+    port=${BASH_REMATCH[4]}
+    path=${BASH_REMATCH[5]}
+    if [ -z "$other_args" ]; then
+        cmd="$termenv ssh -t $userat$host -p $port \"cd $path && exec "'\$SHELL'"\" && exec $SHELL"
+    else
+        cmd="$termenv ssh -t $userat$host -p $port \"cd $path &&  $other_args; \$SHELL -i -l\" "
+    fi
+    if [ -z "$tmux_session" ]; then
+        eval  $TERMINAL $term_args $TERMINAL_EXEC  $SHELL  -i -c \'$cmd\'
+    else
+        $TERMINAL $term_args $TERMINAL_EXEC tmux.sh --session $tmux_session --cwd "$working_directory" "--" $SHELL -i -c \'$cmd\'
+    fi
 else
     if [ -n "$working_directory" ]; then
         term_args=" $term_args $WORKING_DIRECTORY_ARG=$working_directory"
