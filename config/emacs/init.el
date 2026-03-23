9d977aab32a966f31c0514761c359815379c4ff9
jixiuf <jixiuf@qq.com>
2026-03-23 15:16:35 +0800

    up

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -7,7 +7,7 @@
 (load-theme 'modus-vivendi)
 (require 'conf-gpg)
 (when (featurep 'reka)  (require 'conf-wayland))
-(when (featurep 'ewm)  (require 'conf-ewm))
+(with-eval-after-load 'ewm (require 'conf-ewm))
 (defun lazy-load-gpg(&optional f)
   (when (member (frame-live-p f) '(pgtk x ns))
     (unless (featurep 'conf-private)
