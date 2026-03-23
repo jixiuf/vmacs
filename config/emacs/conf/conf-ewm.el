4deea094a23ab5ab7925402696348baa36cd0c47
jixiuf <jixiuf@qq.com>
2026-03-23 23:17:54 +0800

    up

--- config/emacs/conf/conf-ewm.el
+++ config/emacs/conf/conf-ewm.el
@@ -5,45 +5,7 @@
 (setenv "EWM_MODULE_PATH" (expand-file-name "~/repos/ewm/compositor/target/debug/libewm_core.so"))
 (setenv "XDG_CONFIG_HOME" (expand-file-name "~/.config"))
 (when (string-equal (getenv "XDG_SESSION_DESKTOP") "ewm")
-  (require 'ewm)
-  (setenv "XDG_SESSION_DESKTOP" "ewm")
-  (shell-command "/usr/bin/dbus-update-activation-environment --systemd --all;")
-  ;; /usr/local/bin/xremap-ewm /home/jixiuf/.config/xremap/xremap.yaml /home/jixiuf/.config/xremap/xremap-ewm.yaml --watch=device --ignore=dotool keyboard --ignore=Yubico YubiKey OTP+CCID
-
-  ;; /usr/bin/dbus-update-activation-environment --systemd --all
-  ;; EWM compositor must start immediately in daemon mode (runs on TTY)
-  ;; It doesn't need a graphical frame - the compositor creates the display
-  (defun vmacs-ewm-init()
-    ;(ewm-start-module)
-    (defun ewm-key (key)
-      (add-to-list 'ewm-intercept-prefixes `(,(kbd key) :fullscreen))
-      (kbd key))
-
-    (require 'lazy-wayland)
-    (setq wayland-compositor 'ewm)
-    (global-set-key (ewm-key "s-C-f") (wayland-run-or-raise :name firefox :app-id (rx (or "firefox" "firefox-bin" "firefox-esr")) :command "firefox-bin"))
-    (global-set-key (ewm-key "s-C-d") (wayland-run-or-raise :name term :app-id (rx (or "foot" "alacritty" "foot-ws")) :command "alacritty"))
-    
-    (start-process-shell-command
-     "import-environment" nil
-     "systemctl --user import-environment XCURSOR_SIZE GTK_THEME SSH_AUTH_SOCK GPG_TTY DISPLAY  WAYLAND_DISPLAY  XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP XDG_RUNTIME_DIR QT_QPA_PLATFORM _JAVA_AWT_WM_NONREPARENTING SDL_VIDEODRIVER")
-    (start-process-shell-command
-     "dbus-update-activation-environment" nil
-     "dbus-update-activation-environment --systemd --all")
-    ;; (start-process-shell-command     "xremap" " *xremap*"     "/usr/local/bin/xremap-ewm ~/.config/xremap/xremap.yaml ~/.config/xremap/xremap-ewm.yaml --watch=device --ignore='dotool keyboard' ")
-    (start-process-shell-command     "kpmenu" nil     "kpmenu --daemon")
-    (start-process-shell-command     "swaylock" nil     "pidof swaylock || swaylock")
-    (start-process-shell-command     "hypridle" nil     "pidof hypridle||hypridle")
-    (start-process-shell-command     "wl-paste" nil     "pidof wl-paste ||wl-paste --watch cliphist-store&")
-    (start-process-shell-command     "dex" nil     "dex -a -s /etc/xdg/autostart/:~/.config/autostart/")
-    (start-process-shell-command     "sway-session" nil     "systemctl --user restart sway-session.target")
-    (start-process-shell-command     "xremap" " *xremap*"     "systemctl --user restart xremap")
-    )
-  (vmacs-ewm-init)
-
-  (add-hook 'after-init-hook #'vmacs-ewm-init)
-
-;; niri msg outputs
+  
 ;; Output "Apple Computer Inc Color LCD Unknown" (eDP-1)
 ;;   Current mode: 2880x1800 @ 60.001 Hz (preferred)
 ;;   Variable refresh rate: not supported
@@ -61,7 +23,48 @@
           ;; libinput list-devices                         # system-wide
           ;; Per-device override (exact name from libinput)
           ("ELAN0676:00 04F3:3195 Touchpad" :tap nil :accel-speed -0.2)))
-  (ewm--send-input-config)
+
+  (require 'ewm)
+  (setenv "XDG_SESSION_DESKTOP" "ewm")
+  (shell-command "/usr/bin/dbus-update-activation-environment --systemd --all;")
+  ;; /usr/local/bin/xremap-ewm /home/jixiuf/.config/xremap/xremap.yaml /home/jixiuf/.config/xremap/xremap-ewm.yaml --watch=device --ignore=dotool keyboard --ignore=Yubico YubiKey OTP+CCID
+
+  ;; /usr/bin/dbus-update-activation-environment --systemd --all
+  ;; EWM compositor must start immediately in daemon mode (runs on TTY)
+  ;; It doesn't need a graphical frame - the compositor creates the display
+  (defun vmacs-ewm-init()
+    (ewm-start-module)
+    (defun ewm-key (key)
+      (add-to-list 'ewm-intercept-prefixes `(,(kbd key) :fullscreen))
+      (kbd key))
+
+    (require 'lazy-wayland)
+    (setq wayland-compositor 'ewm)
+    (global-set-key (ewm-key "s-C-f") (wayland-run-or-raise :name firefox :app-id (rx (or "firefox" "firefox-bin" "firefox-esr")) :command "firefox-bin"))
+    (global-set-key (ewm-key "s-C-d") (wayland-run-or-raise :name term :app-id (rx (or "foot" "alacritty" "foot-ws")) :command "alacritty"))
+    (ewm--send-intercept-keys)
+    
+    (start-process-shell-command
+     "import-environment" nil
+     "systemctl --user import-environment XCURSOR_SIZE GTK_THEME SSH_AUTH_SOCK GPG_TTY DISPLAY  WAYLAND_DISPLAY  XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP XDG_RUNTIME_DIR QT_QPA_PLATFORM _JAVA_AWT_WM_NONREPARENTING SDL_VIDEODRIVER")
+    (start-process-shell-command
+     "dbus-update-activation-environment" nil
+     "dbus-update-activation-environment --systemd --all")
+    ;; (start-process-shell-command     "xremap" " *xremap*"     "/usr/local/bin/xremap-ewm ~/.config/xremap/xremap.yaml ~/.config/xremap/xremap-ewm.yaml --watch=device --ignore='dotool keyboard' ")
+    (start-process-shell-command     "kpmenu" nil     "kpmenu --daemon")
+    (start-process-shell-command     "swaylock" nil     "pidof swaylock || swaylock")
+    (start-process-shell-command     "hypridle" nil     "pidof hypridle||hypridle")
+    (start-process-shell-command     "wl-paste" nil     "pidof wl-paste ||wl-paste --watch cliphist-store&")
+    (start-process-shell-command     "dex" nil     "dex -a -s /etc/xdg/autostart/:~/.config/autostart/")
+    (start-process-shell-command     "sway-session" nil     "systemctl --user restart sway-session.target")
+    (start-process-shell-command     "xremap" " *xremap*"     "systemctl --user restart xremap")
+    )
+  (when after-init-time
+    (vmacs-ewm-init)
+  (add-hook 'after-init-hook #'vmacs-ewm-init))
+
+
+;; niri msg outputs
 
   (defvar consult-source-xdg-apps
     `(:name "Apps"
