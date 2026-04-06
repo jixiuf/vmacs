24bc92e5304df4195b37f6aec4d3e3bcf6a7a639
jixiuf <jixiuf@qq.com>
2026-04-06 19:44:14 +0800

    up

--- config/emacs/conf/conf-term.el
+++ config/emacs/conf/conf-term.el
@@ -8,8 +8,6 @@
 
 (defun vmacs-ghostel-self-insert()
   (interactive)
-  ;; (unless (evil-insert-state-p)
-  ;;   (evil-insert-state))
   (unless (bray-state-derived-p 'insert)
     (bray-state-stack-push meep-state-insert))
   (call-interactively 'ghostel--self-insert))
@@ -22,12 +20,13 @@
 
 (defun vmacs-ghostel-enable-copy()
   (when (member major-mode '(ghostel-mode))
-    (ghostel-copy-mode)
-    (ghostel--set-cursor-style 1 t)
-    ))
+    (when (member this-command '(ghostel ghostel-other))
+      (ghostel-copy-mode)
+      (ghostel--set-cursor-style 1 t))))
 
 (add-hook 'meep-state-hook-insert-enter 'vmacs-ghostel-disable-copy)
 (add-hook 'meep-state-hook-normal-enter 'vmacs-ghostel-enable-copy)
+
 (provide 'conf-term)
 
 ;; Local Variables:
