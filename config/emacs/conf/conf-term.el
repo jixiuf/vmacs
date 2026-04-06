46991ee8f3ce4a19c27550621b8e384457d6507d
jixiuf <jixiuf@qq.com>
2026-04-06 22:19:26 +0800

    up

--- config/emacs/conf/conf-term.el
+++ config/emacs/conf/conf-term.el
@@ -3,29 +3,35 @@
 ;;; Code:
 
 (define-key ghostel-mode-map (kbd "C-,") #'ghostel-other)
+(define-key ghostel-mode-map (kbd "C-s-v") #'ghostel-yank)
 (setq-default term-prompt-regexp "^[^#$%>\n]*[#$%>] *") ;默认 regex 相当于没定义，term-bol 无法正常中转到开头处
 (setq ghostel-enable-osc52 t)
 
-(defun vmacs-ghostel-self-insert()
-  (interactive)
-  (unless (bray-state-derived-p 'insert)
-    (bray-state-stack-push meep-state-insert))
-  (call-interactively 'ghostel--self-insert))
 
 
 (defun vmacs-ghostel-disable-copy()
   (when (member major-mode '(ghostel-mode))
     (ghostel-copy-mode-exit)
+    (display-line-numbers-mode)
+    (setq display-line-numbers 'absolute)
     (ghostel--set-cursor-style 0 t)))
 
 (defun vmacs-ghostel-enable-copy()
   (when (member major-mode '(ghostel-mode))
+    (display-line-numbers-mode)
+    (setq display-line-numbers nil)
     (when (member this-command '(ghostel ghostel-other))
       (ghostel-copy-mode)
       (ghostel--set-cursor-style 1 t))))
 
 (add-hook 'meep-state-hook-insert-enter 'vmacs-ghostel-disable-copy)
 (add-hook 'meep-state-hook-normal-enter 'vmacs-ghostel-enable-copy)
+
+;; (defun vmacs-ghostel-self-insert()
+;;   (interactive)
+;;   (unless (bray-state-derived-p 'insert)
+;;     (bray-state-stack-push meep-state-insert))
+;;   (call-interactively 'ghostel--self-insert))
 
 (provide 'conf-term)
 
