b3f14d5fbe4c156d418f91200d89ac5651469193
jixiuf <jixiuf@qq.com>
2026-06-12 10:55:45 +0800

    up

--- mac/LaunchAgents/git_pull_master.sh
+++ mac/LaunchAgents/git_pull_master.sh
@@ -6,31 +6,25 @@ cd ~/.emacs.d/&& git pull --commit origin master:master && git push origin maste
 # cd ~/.emacs.d/&& make pull
 # cd ~/.emacs.d/&& make push
 # cd ~/.emacs.d/&& make compile
-if [ -d ~/Documents/org ]; then
-    cd ~/Documents/org && git fetch --all
-    cd ~/Documents/org && git pull --commit &&git push
-fi
+# if [ -d ~/Documents/org ]; then
+#     cd ~/Documents/org && git fetch --all
+#     cd ~/Documents/org && git pull --commit &&git push
+# fi
 
-if [ -d ~/repos/dotfiles/ ]; then
-    cd ~/repos/dotfiles/  &&git pull --commit &&git push
-fi
+# if [ -d ~/repos/dotfiles/ ]; then
+#     cd ~/repos/dotfiles/  &&git pull --commit &&git push
+# fi
 if [ -d ~/repos/emacs/ ]; then
     cd ~/repos/emacs/  &&git pull
 fi
-if [ -d ~/repos/magit ]; then
-    cd ~/repos/magit && git pull
-fi
-if [ -d ~/repos/libegit2 ]; then
-    cd ~/repos/libegit2 && git pull
-fi
-if [ -d ~/go/src/golang.org/x/tools ]; then
-    cd ~/go/src/golang.org/x/tools/gopls; git pull ;GO111MODULE=on go install
-fi
-export HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK=1
-brew update
-HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles   brew upgrade
-HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles   brew upgrade
-HOMEBREW_BOTTLE_DOMAIN= brew upgrade
+# if [ -d ~/go/src/golang.org/x/tools ]; then
+#     cd ~/go/src/golang.org/x/tools/gopls; git pull ;GO111MODULE=on go install
+# fi
+# export HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK=1
+# brew update
+# HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles   brew upgrade
+# HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles   brew upgrade
+# HOMEBREW_BOTTLE_DOMAIN= brew upgrade
 find ~/repos -depth 1 -type d  -exec sh -c 'cd {}&&git pull;cd ..'  \;
 
 echo "git_pull_master agent launched at " `date`
