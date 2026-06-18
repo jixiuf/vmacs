44d8ffba81195d1a59f4293cfb6ff790a5a69d88
jixiuf <jixiuf@qq.com>
2026-06-18 16:08:51 +0800

    clean pkg

--- config/emacs/conf/conf-package.el
+++ config/emacs/conf/conf-package.el
@@ -9,27 +9,9 @@
 ;; (package-initialize)
 (add-hook 'after-init-hook (lambda()
                              (package-install-selected-packages t)))
-;; (defun vmacs-package-hook()
-;;   (meep-local-set-key "/" #'consult-focus-lines)
-;;   (meep-local-set-key "z" #'consult-hide-lines))
 
-;; (add-hook 'package-menu-mode-hook #'vmacs-package-hook)
-(setq treesit-language-source-alist
-      '((go . ("https://github.com/tree-sitter/tree-sitter-go.git" "v0.25.0"))
-	    (gomod . ("https://github.com/camdencheek/tree-sitter-go-mod.git" "v1.1.0"))
-        (java . ("https://github.com/tree-sitter/tree-sitter-java.git" "v0.23.5"))
-	    (rust . ("https://github.com/tree-sitter/tree-sitter-rust.git" "v0.23.2"))
-	    ;; (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile.git" "v0.2.0"))
-	    (yaml . ("https://github.com/ikatyang/tree-sitter-yaml.git" "v0.5.0"))
-        (json . ("https://github.com/tree-sitter/tree-sitter-json.git" "v0.24.8"))
-	    (c . ("https://github.com/tree-sitter/tree-sitter-c.git" "v0.23.5"))
-	    (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp.git" "v0.23.0"))))
-
-;; Install missing grammars.
-(mapc #'(lambda (lang)
-	      (unless (treesit-language-available-p lang)
-	        (treesit-install-language-grammar lang)))
-      (mapcar #'car treesit-language-source-alist))
+(setq treesit-auto-install-grammar t) ; EMACS-31
+(setq treesit-enabled-modes t)        ; EMACS-31
 
 (provide 'conf-package)
 
